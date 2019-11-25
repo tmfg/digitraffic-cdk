638d782e02e03ea9fd8bf3e58c2684b57df6cbea
Massimo <massimo.prencipe@solita.fi>
2019-11-25 14:17:46 +0200

    Add mock implementation

--- open311-cdk/lib/open311-cdk-stack.ts
+++ open311-cdk/lib/open311-cdk-stack.ts
@@ -1,37 +1,50 @@
 import cdk = require('@aws-cdk/core');
 import apigateway = require('@aws-cdk/aws-apigateway');
-import iam = require('@aws-cdk/aws-iam');
 
-import * as integrationSwagger from './open311-swagger-integration';
-import * as publicSwagger from './open311-swagger-public';
+import {MockIntegration, PassthroughBehavior} from "@aws-cdk/aws-apigateway";
 
 export class Open311CdkStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const role = new iam.Role(this, 'APIGatewayPushToCloudWatchLogsRole', {
-            managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonAPIGatewayPushToCloudWatchLogs')],
-            assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com')
+        const integrationApi = new apigateway.RestApi(this, 'Open311-integration', {
+            restApiName: 'Open311 integration API',
         });
 
-        new apigateway.CfnAccount(this, 'account', {
-            cloudWatchRoleArn: role.roleArn
+        const requests = integrationApi.root.addResource("requests");
+        requests.addMethod("POST", new MockIntegration({
+            passthroughBehavior: PassthroughBehavior.WHEN_NO_MATCH,
+            requestTemplates: {
+                'application/json': JSON.stringify({statusCode: 200})
+            },
+            integrationResponses: [
+                {
+                    statusCode: '200'
+                }
+            ]
+        }), {
+            methodResponses: [
+                {
+                    statusCode: '200'
+                }
+            ]
         });
-
-        new apigateway.CfnRestApi(this, 'Open311-integration', {
-            body: integrationSwagger.default,
-            description: 'Open311 integration API',
-            endpointConfiguration: {
-                types: ['REGIONAL']
-            }
-        });
-
-        new apigateway.CfnRestApi(this, 'Open311-public', {
-            body: publicSwagger.default,
-            description: 'Open311 public API',
-            endpointConfiguration: {
-                types: ['REGIONAL']
-            }
+        requests.addMethod("PUT", new MockIntegration({
+            passthroughBehavior: PassthroughBehavior.WHEN_NO_MATCH,
+            requestTemplates: {
+                'application/json': JSON.stringify({statusCode: 200})
+            },
+            integrationResponses: [
+                {
+                    statusCode: '200'
+                }
+            ]
+        }), {
+            methodResponses: [
+                {
+                    statusCode: '200'
+                }
+            ]
         });
     }
 }
