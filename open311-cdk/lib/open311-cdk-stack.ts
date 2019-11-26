8364d2a4ec56c7bad4b3188652ba0e4c75f33b2d
Massimo <massimo.prencipe@solita.fi>
2019-11-26 13:14:30 +0200

    Add security group and policy required for private api

--- open311-cdk/lib/open311-cdk-stack.ts
+++ open311-cdk/lib/open311-cdk-stack.ts
@@ -1,6 +1,7 @@
 import cdk = require('@aws-cdk/core');
 import apigateway = require('@aws-cdk/aws-apigateway');
 import ec2 = require('@aws-cdk/aws-ec2');
+import iam = require('@aws-cdk/aws-iam');
 
 const lambda = require('@aws-cdk/aws-lambda');
 import {EndpointType, LambdaIntegration} from "@aws-cdk/aws-apigateway";
@@ -17,7 +18,28 @@ export class Open311CdkStack extends cdk.Stack {
         const newRequestIntegration = new LambdaIntegration(newRequestHandler);
         const integrationApi = new apigateway.RestApi(this, 'Open311-integration', {
             restApiName: 'Open311 integration API',
-            endpointTypes: [EndpointType.PRIVATE]
+            endpointTypes: [EndpointType.PRIVATE],
+            policy: new iam.PolicyDocument({
+                statements: [
+                    new iam.PolicyStatement({
+                        effect: iam.Effect.ALLOW,
+                        actions: [
+                            "execute-api:Invoke"
+                        ],
+                        resources: [
+                            "*"
+                        ],
+                        conditions: {
+                            "StringEquals": {
+                                "aws:sourceVpc": process.env.VPC_ID as string
+                            }
+                        },
+                        principals: [
+                            new iam.AnyPrincipal()
+                        ]
+                    })
+                ]
+            })
         });
         const requests = integrationApi.root.addResource("requests");
         requests.addMethod("POST", newRequestIntegration, {});
@@ -27,11 +49,17 @@ export class Open311CdkStack extends cdk.Stack {
             availabilityZones: (process.env.VPC_AZS as string).split(','),
             privateSubnetIds: (process.env.VPC_SUBNETS as string).split(',')
         });
+        const apiGwEndpointSg = new ec2.SecurityGroup(this, 'APIGatewayEndpointSG', {
+            vpc
+        });
+        apiGwEndpointSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
+        apiGwEndpointSg.addIngressRule(ec2.Peer.anyIpv6(), ec2.Port.tcp(443));
         new ec2.InterfaceVpcEndpoint(this, 'APIGatewayEndpoint', {
             vpc: vpc,
             service: ec2.InterfaceVpcEndpointAwsService.APIGATEWAY,
             privateDnsEnabled: true,
-            subnets: {subnets: vpc.privateSubnets}
+            subnets: {subnets: vpc.privateSubnets},
+            securityGroups: [apiGwEndpointSg]
         });
     }
 }
