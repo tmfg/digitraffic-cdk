e50b0bd979a96d0e50fd5746db1c6434ce0dacbb
Massimo <massimo.prencipe@solita.fi>
2019-11-26 08:56:00 +0200

    Make api private

--- open311-cdk/lib/open311-cdk-stack.ts
+++ open311-cdk/lib/open311-cdk-stack.ts
@@ -2,7 +2,7 @@ import cdk = require('@aws-cdk/core');
 import apigateway = require('@aws-cdk/aws-apigateway');
 
 const lambda = require('@aws-cdk/aws-lambda');
-import {JsonSchemaType, JsonSchemaVersion, LambdaIntegration} from "@aws-cdk/aws-apigateway";
+import {EndpointType, LambdaIntegration} from "@aws-cdk/aws-apigateway";
 
 export class Open311CdkStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -16,6 +16,7 @@ export class Open311CdkStack extends cdk.Stack {
         const newRequestIntegration = new LambdaIntegration(newRequestHandler);
         const integrationApi = new apigateway.RestApi(this, 'Open311-integration', {
             restApiName: 'Open311 integration API',
+            endpointTypes: [EndpointType.PRIVATE]
         });
         const requests = integrationApi.root.addResource("requests");
         requests.addMethod("POST", newRequestIntegration, {
