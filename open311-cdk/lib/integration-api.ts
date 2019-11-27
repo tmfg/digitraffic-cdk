ab51c89826ff43299b19e46ca7c8d333ff5b8e21
Massimo <massimo.prencipe@solita.fi>
2019-11-27 20:43:33 +0200

    Add public open311 api, separate apis to own files

---
+++ open311-cdk/lib/integration-api.ts
@@ -0,0 +1,56 @@
+import apigateway = require('@aws-cdk/aws-apigateway');
+import iam = require('@aws-cdk/aws-iam');
+const lambda = require('@aws-cdk/aws-lambda');
+import {EndpointType, LambdaIntegration} from "@aws-cdk/aws-apigateway";
+import * as TestStackProps from "./stackprops-test";
+import {Construct} from "@aws-cdk/core";
+
+export function create(stack: Construct) {
+    const newRequestHandler = new lambda.Function(stack, 'NewRequestLambda', {
+        code: new lambda.AssetCode('lib'),
+        handler: 'newrequest.handler',
+        runtime: lambda.Runtime.NODEJS_10_X
+    });
+    const newRequestIntegration = new LambdaIntegration(newRequestHandler);
+
+    const integrationApi = new apigateway.RestApi(stack, 'Open311-integration', {
+        deployOptions: {
+            loggingLevel: apigateway.MethodLoggingLevel.ERROR,
+        },
+        restApiName: 'Open311 integration API',
+        endpointTypes: [EndpointType.PRIVATE],
+        policy: new iam.PolicyDocument({
+            statements: [
+                new iam.PolicyStatement({
+                    effect: iam.Effect.ALLOW,
+                    actions: [
+                        "execute-api:Invoke"
+                    ],
+                    resources: [
+                        "*"
+                    ],
+                    conditions: {
+                        "StringEquals": {
+                            "aws:sourceVpc": TestStackProps.default.vpcId
+                        }
+                    },
+                    principals: [
+                        new iam.AnyPrincipal()
+                    ]
+                })
+            ]
+        })
+    });
+    const requests = integrationApi.root.addResource("requests");
+    requests.addMethod("POST", newRequestIntegration, {
+        apiKeyRequired: true
+    });
+    const apiKey = integrationApi.addApiKey('Integration API key');
+    const plan = integrationApi.addUsagePlan('Integration Usage Plan', {
+        name: 'Integration Usage Plan',
+        apiKey
+    });
+    plan.addApiStage({
+        stage: integrationApi.deploymentStage
+    });
+}
--- open311-cdk/lib/open311-cdk-stack.ts
+++ open311-cdk/lib/open311-cdk-stack.ts
@@ -1,61 +1,14 @@
 import cdk = require('@aws-cdk/core');
-import apigateway = require('@aws-cdk/aws-apigateway');
-import iam = require('@aws-cdk/aws-iam');
-const lambda = require('@aws-cdk/aws-lambda');
-import {EndpointType, LambdaIntegration} from "@aws-cdk/aws-apigateway";
+import * as IntegrationApi from './integration-api';
+import * as PublicApi from './public-api';
 import * as TestStackProps from './stackprops-test';
 
 export class Open311CdkStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const newRequestHandler = new lambda.Function(this, 'NewRequestLambda', {
-            code: new lambda.AssetCode('lib'),
-            handler: 'newrequest.handler',
-            runtime: lambda.Runtime.NODEJS_10_X
-        });
-        const newRequestIntegration = new LambdaIntegration(newRequestHandler);
-
-        const integrationApi = new apigateway.RestApi(this, 'Open311-integration', {
-            deployOptions: {
-                loggingLevel: apigateway.MethodLoggingLevel.ERROR,
-            },
-            restApiName: 'Open311 integration API',
-            endpointTypes: [EndpointType.PRIVATE],
-            policy: new iam.PolicyDocument({
-                statements: [
-                    new iam.PolicyStatement({
-                        effect: iam.Effect.ALLOW,
-                        actions: [
-                            "execute-api:Invoke"
-                        ],
-                        resources: [
-                            "*"
-                        ],
-                        conditions: {
-                            "StringEquals": {
-                                "aws:sourceVpc": TestStackProps.default.vpcId
-                            }
-                        },
-                        principals: [
-                            new iam.AnyPrincipal()
-                        ]
-                    })
-                ]
-            })
-        });
-        const requests = integrationApi.root.addResource("requests");
-        requests.addMethod("POST", newRequestIntegration, {
-            apiKeyRequired: true
-        });
-        const apiKey = integrationApi.addApiKey('Integration API key');
-        const plan = integrationApi.addUsagePlan('Integration Usage Plan', {
-           name: 'Integration Usage Plan',
-           apiKey
-        });
-        plan.addApiStage({
-            stage: integrationApi.deploymentStage
-        });
+        IntegrationApi.create(this);
+        PublicApi.create(this);
     }
 }
 
