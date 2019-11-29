0eb4e57bcaadeacf3070829d1f1ad865b1af008a
Massimo <massimo.prencipe@solita.fi>
2019-11-29 11:49:56 +0200

    Fix malformed response

--- open311-cdk/lib/lambda-get-request.ts
+++ open311-cdk/lib/lambda-get-request.ts
@@ -2,5 +2,5 @@ import {APIGatewayEvent} from 'aws-lambda';
 
 export const handler = async (event: APIGatewayEvent) : Promise <any> => {
     console.log('Received request for id ' + event.pathParameters?.['request_id']);
-    return { statusCode: 200, request: null };
+    return { statusCode: 200, request: JSON.stringify(null) };
 };
--- open311-cdk/lib/lambda-get-service.ts
+++ open311-cdk/lib/lambda-get-service.ts
@@ -2,5 +2,5 @@ import {APIGatewayEvent} from 'aws-lambda';
 
 export const handler = async (event: APIGatewayEvent) : Promise <any> => {
     console.log('Received request for service id ' + event.pathParameters?.['service_id']);
-    return { statusCode: 200, service: null };
+    return { statusCode: 200, service: JSON.stringify(null) };
 };
