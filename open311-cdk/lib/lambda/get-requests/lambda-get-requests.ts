59bb60fa516462e968089e98a5a3396c44325a69
Massimo <massimo.prencipe@solita.fi>
2019-11-30 15:22:35 +0200

    DPO-923 select geometry as long,lat

--- open311-cdk/lib/lambda/get-requests/lambda-get-requests.ts
+++ open311-cdk/lib/lambda/get-requests/lambda-get-requests.ts
@@ -1,13 +1,34 @@
 import {APIGatewayEvent} from 'aws-lambda';
 import {initDb} from 'digitraffic-lambda-postgres/database';
 
-export const handler = async (event: APIGatewayEvent) : Promise <any> => {
+export const handler = async (event: APIGatewayEvent): Promise<any> => {
     const db = initDb(
         process.env.DB_USER as string,
         process.env.DB_PASS as string,
         process.env.DB_URI as string
     );
-    const requests = await db.many("SELECT * FROM open311_service_request");
+    const requests = await db.many(`
+        SELECT service_request_id,
+               status,
+               status_notes,
+               service_name,
+               service_code,
+               description,
+               agency_responsible,
+               service_notice,
+               requested_datetime,
+               updated_datetime,
+               expected_datetime,
+               address,
+               address_id,
+               zipcode,
+               ST_X(geometry) AS long,
+               ST_Y(geometry) AS lat,
+               media_url
+        FROM open311_service_request
+    `);
 
-    return { statusCode: 200, body: JSON.stringify(requests) };
+    db.$pool.end();
+
+    return {statusCode: 200, body: JSON.stringify(requests)};
 };
---
+++ open311-cdk/lib/model/service.ts
@@ -0,0 +1,15 @@
+export enum ServiceType {
+    realtime,
+    batch,
+    blackbox
+}
+
+export interface Service {
+    service_code: string;
+    service_name: string;
+    description: string;
+    metadata: boolean;
+    type: ServiceType;
+    keywords: string;
+    group: string;
+}
