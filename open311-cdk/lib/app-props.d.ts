51c15531b7703ec246d5c00fafa4cb28c0a895c6
Massimo <massimo.prencipe@solita.fi>
2019-12-03 09:31:28 +0200

    DPO-923 add props file

---
+++ open311-cdk/lib/app-props.d.ts
@@ -0,0 +1,13 @@
+declare interface Props {
+    vpcId: string;
+    privateSubnetIds: string[];
+    availabilityZones: string[];
+    lambdaDbSgId: string;
+    dbProps: DbProps;
+    defaultLambdaDurationSeconds: number;
+}
+declare interface DbProps {
+    username: string;
+    password: string;
+    uri: string;
+}
