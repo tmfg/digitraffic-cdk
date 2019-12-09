48dd3196bd55f51262be8a3395482a9303160454
Massimo <massimo.prencipe@solita.fi>
2019-12-09 15:57:32 +0200

    DPO-923 add test for checking orphan service requests

---
+++ open311-cdk/test/lib/lambda/check-orphan-requests/check-orphan-requests.test.ts
@@ -0,0 +1,21 @@
+import * as pgPromise from "pg-promise";
+import {handler} from '../../../../lib/lambda/check-orphan-requests/lambda-check-orphan-requests';
+import {dbTestBase, insertServiceRequest} from "../../db-testutil";
+import {findAllServiceCodes} from "../../../../lib/db/db-services";
+import {newServiceRequest} from "../../testdata";
+import * as aws from 'aws-sdk-mock';
+import * as sinon from 'sinon';
+
+describe('check-orphan-requests', dbTestBase((db: pgPromise.IDatabase<any, any>) => {
+
+    test('check', async () => {
+        await insertServiceRequest(db, [newServiceRequest()]);
+        const publishSpy = sinon.spy();
+        aws.mock('SNS', 'publish', publishSpy);
+
+        await handler();
+
+        expect(publishSpy.calledOnce);
+    });
+
+}));
