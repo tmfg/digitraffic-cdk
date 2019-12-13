51858859151517a73ba284b5fe6f99f89fd51da2
Teijo Rönkä <teijo.ronka@solita.fi>
2019-12-13 17:16:31 +0200

    DPO-975 fix async, fix testutil

--- nordicway2-cdk/test/lib/lambda/update-annotations/update-annotations.test.ts
+++ nordicway2-cdk/test/lib/lambda/update-annotations/update-annotations.test.ts
@@ -1,10 +1,10 @@
 import * as pgPromise from "pg-promise";
 import {handler} from "../../../../lib/lambda/update-annotations/lambda-update-annotations";
-import {dbTestBase, insertServiceRequest} from "../../db-testutil";
+import {dbTestBase} from "../../db-testutil";
 
 describe('update-annotations', dbTestBase((db: pgPromise.IDatabase<any,any>) => {
 
-    test('Multiple service requests - created', async () => {
+    test('Test', async () => {
         const response = await handler();
 
         expect(response.statusCode).toBe(200);
