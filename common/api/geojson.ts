964507cdcbae104425988b858049aa3735e7572a
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-03-24 11:33:45 +0200

    Update deps

--- common/api/geojson.ts
+++ common/api/geojson.ts
@@ -1,6 +1,6 @@
 import {FeatureCollection,Feature,GeoJsonProperties} from "geojson";
 
-export function createFeatureCollection(features: Feature[], lastUpdated: Date | null) {
+export function createFeatureCollection(features: Feature[], lastUpdated: Date | null): FeatureCollection {
     return <FeatureCollection> {
         type: "FeatureCollection",
         lastUpdated: lastUpdated,
