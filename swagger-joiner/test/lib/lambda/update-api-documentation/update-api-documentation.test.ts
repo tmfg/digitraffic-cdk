c13efd721766d9370c909c57314c6c1f53aa844c
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-04-01 09:37:57 +0300

    DPO-1068: handle case for no versions

--- swagger-joiner/test/lib/lambda/update-api-documentation/update-api-documentation.test.ts
+++ swagger-joiner/test/lib/lambda/update-api-documentation/update-api-documentation.test.ts
@@ -3,7 +3,7 @@ import {ListOfDocumentationVersion} from "aws-sdk/clients/apigateway";
 
 describe('update-api-documentation', () => {
 
-    test('getLatestVersion', async () => {
+    test('getLatestVersion - existing versions', async () => {
         // random length array with random versions
         const docVersions: ListOfDocumentationVersion = Array.from({length: Math.ceil(Math.random() * 10)})
             .map(() => ({version: (Math.ceil(Math.random() * 10)).toString()}));
@@ -11,6 +11,13 @@ describe('update-api-documentation', () => {
         docVersions.sort((d1, d2) => Number(d2.version) - Number(d1.version));
 
         expect(latest).toBe(Number(docVersions[0].version));
+    });
+
+    test('getLatestVersion - no versions', async () => {
+        // random length array with random versions
+        const latest = await getLatestVersion([]);
+
+        expect(latest).toBe(1);
     });
 
 });
