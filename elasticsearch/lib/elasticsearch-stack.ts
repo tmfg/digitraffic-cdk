55b0ab079f8b7340ea8f49b55dc512e6c6bdc6d8
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2020-04-24 09:21:21 +0300

    DPO-861 base

---
+++ elasticsearch/lib/elasticsearch-stack.ts
@@ -0,0 +1,59 @@
+import core = require('@aws-cdk/core');
+import {CfnDomain} from "@aws-cdk/aws-elasticsearch";
+
+export class ElasticsearchStack extends core.Stack {
+    constructor(scope: core.App, id: string, props?: core.StackProps,) {
+        super(scope, id, {
+            env: {
+                account: '',
+                region: 'eu-west-1'
+            }
+        });
+
+        const esVersion: string = '6.8';
+        const esId = 'dt-elasticsearch';
+        const domainName = 'dt-elasticsearch-domain';
+
+        const domain = new CfnDomain(this, esId, {
+            accessPolicies: {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "",
+                        "Effect": "Allow",
+                        "Principal": {
+                            "AWS": "*"
+                        },
+                        "Action": "es:*",
+                        "Resource": "arn:aws:es:eu-west-1:111964331789:domain/dt-elasticsearch-domain/*",
+                        "Condition": {
+                            "IpAddress": {
+                                "aws:SourceIp": []
+                            }
+                        }
+                    },
+                    {
+                        "Sid": "",
+                        "Effect": "Allow",
+                        "Principal": {
+                            "AWS": []
+                        },
+                        "Action": "es:*",
+                        "Resource": "arn:aws:es:eu-west-1:111964331789:domain/dt-elasticsearch-domain/*"
+                    }
+                ]
+            },
+            domainName: domainName,
+            ebsOptions: {
+                ebsEnabled: true,
+                volumeSize: 200,
+                volumeType: 'gp2',
+            },
+            elasticsearchClusterConfig: {
+                instanceCount: 1,
+                instanceType: 'r5.large.elasticsearch'
+            },
+            elasticsearchVersion: esVersion
+        });
+    }
+}
