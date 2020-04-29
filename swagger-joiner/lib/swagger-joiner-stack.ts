0789b5ac5299cbe1cad6995be9c40832d5bfcf13
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-04-29 09:07:37 +0300

    DPO-1068 add CORS so site can retrieve Swagger spec

--- swagger-joiner/lib/swagger-joiner-stack.ts
+++ swagger-joiner/lib/swagger-joiner-stack.ts
@@ -1,7 +1,7 @@
 import {Construct, Stack, StackProps} from '@aws-cdk/core';
 import {Props} from './app-props';
 import * as InternalLambdas from './internal-lambdas'
-import {Bucket} from "@aws-cdk/aws-s3";
+import {Bucket, HttpMethods} from "@aws-cdk/aws-s3";
 import {Effect, PolicyStatement} from "@aws-cdk/aws-iam";
 
 export class SwaggerJoinerStack extends Stack {
@@ -14,7 +14,16 @@ export class SwaggerJoinerStack extends Stack {
 
     private createBucket(swaggerJoinerProps: Props) {
         const bucket = new Bucket(this, 'SwaggerBucket', {
-            bucketName: swaggerJoinerProps.bucketName
+            bucketName: swaggerJoinerProps.bucketName,
+            cors: [{
+                allowedOrigins: [
+                    'https://tie.digitraffic.fi',
+                    'https://tie-test.digitraffic.fi',
+                    'https://meri.digitraffic.fi',
+                    'https://meri-test.digitraffic.fi'
+                ],
+                allowedMethods: [HttpMethods.GET]
+            }]
         });
 
         const getObjectStatement = new PolicyStatement({
