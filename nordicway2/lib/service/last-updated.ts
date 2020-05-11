0eda97b7b1e3c8c45ed905b5c6ffb26bae0788cd
Teijo Rönkä <teijo.ronka@solita.fi>
2020-05-11 08:50:56 +0300

    use ??

--- nordicway2/lib/service/last-updated.ts
+++ nordicway2/lib/service/last-updated.ts
@@ -6,6 +6,6 @@ export function lastUpdated() {
     return inDatabase(async (db: pgPromise.IDatabase<any,any>) => {
         const timestamp = await LastUpdatedDB.getLastUpdated(db, 'NW2_ANNOTATIONS');
 
-        return timestamp == null ? new Date() : timestamp;
+        return timestamp ?? new Date();
     });
 }
