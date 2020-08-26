df3d83f737f4abeac0b65a918376c8106a65c316
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-08-26 07:40:40 +0300

    DPO-1039 add SQS event records size logging

--- portcall-estimates/lib/lambda/process-queue/lambda-process-queue.ts
+++ portcall-estimates/lib/lambda/process-queue/lambda-process-queue.ts
@@ -2,6 +2,6 @@ import {saveEstimates} from "../../service/estimates";
 import {ApiEstimate} from "../../model/estimate";
 
 export const handler = async (event: any, context: any): Promise<any> => {
-    console.info('Records size: ', event.Records.length);
+    console.info(`method=portcallEstimatesProcessQueue portCallRecordsReceived=${event.Records.length}`);
     return await saveEstimates(event.Records.map((e: any) => JSON.parse(e.body) as ApiEstimate));
 };
