57fb2497badd91b287a60ffa966951a026409446
Teijo Rönkä <teijo.ronka@solita.fi>
2020-10-13 14:12:47 +0300

    DPO-1182 more tests

--- marine/portcall-estimates/test/lib/subscriptions/db/db-shiplist.test.ts
+++ marine/portcall-estimates/test/lib/subscriptions/db/db-shiplist.test.ts
@@ -1,11 +1,34 @@
 import * as pgPromise from "pg-promise"
 import {findByLocode} from '../../../../lib/subscriptions/db/db-shiplist';
 import {dbTestBase} from "../../db-testutil";
+import {updateEstimate} from "../../../../lib/estimates/db/db-estimates";
+import {EventType} from "../../../../lib/estimates/model/estimate";
+
+const TEST_MMSI = 12345;
 
 describe('shiplists', dbTestBase((db: pgPromise.IDatabase<any,any>) => {
-    test('getSubscriptionList', async () => {
+    test('getSubscriptionList - empty', async () => {
         const subs = await findByLocode(db, 'FIRAU');
 
         expect(subs.length).toBe(0);
+    });
+
+    test('getSubscriptionList - one', async() => {
+        await updateEstimate(db, {
+            eventTime:new Date().toISOString(),
+            eventType:EventType.ETA,
+            eventTimeConfidenceLower:null,
+            eventTimeConfidenceUpper:null,
+            recordTime:new Date().toISOString(),
+            location: { port: "FIRAU" },
+            ship: { mmsi: TEST_MMSI },
+            source: 'test'
+        });
+
+        const subs = await findByLocode(db, 'FIRAU');
+        expect(subs.length).toBe(1);
+
+        const subs2 = await findByLocode(db, 'FIHEL');
+        expect(subs2.length).toBe(0);
     });
 }));
