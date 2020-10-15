8dcb06ce9a2a5de24fa06deb787edf8588c4907c
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-10-15 15:47:04 +0300

    DPO-1039 switch from SQS short to long polling

--- marine/portcall-estimates/lib/estimates/sqs.ts
+++ marine/portcall-estimates/lib/estimates/sqs.ts
@@ -1,16 +1,18 @@
 import {Queue, QueueEncryption} from "@aws-cdk/aws-sqs";
-import {Construct} from "@aws-cdk/core";
+import {Construct, Duration} from "@aws-cdk/core";
 
 export function createQueue(scope: Construct): QueueAndDLQ {
     const queueName = 'PortcallEstimateQueue';
     const dlqQueueName = 'PortcallEstimateQueue-DLQ'
     const dlq = new Queue(scope, dlqQueueName, {
         queueName: dlqQueueName,
+        receiveMessageWaitTime: Duration.seconds(20),
         encryption: QueueEncryption.KMS_MANAGED
     });
     const queue = new Queue(scope, queueName, {
         queueName,
         encryption: QueueEncryption.KMS_MANAGED,
+        receiveMessageWaitTime: Duration.seconds(20),
         deadLetterQueue: {
             maxReceiveCount: 3,
             queue: dlq
