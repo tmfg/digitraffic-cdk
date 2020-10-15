2a399e5f4932a69c5f6254bf8864164d11af3b1d
Jouni Sonninen <jouni.sonninen@solita.fi>
2020-10-15 12:17:23 +0200

    DPO-1154 HARJA työkoneen seurantatiedon vastaanoton korvaaminen lambdalla - FIFO

--- road/maintenance-tracking/test/scripts/post-to-apigateway-lambda.sh
+++ road/maintenance-tracking/test/scripts/post-to-apigateway-lambda.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 FILE=${1:?"Give path to file to post"}
-BASE_URL="https://5xrmdxem70.execute-api.eu-west-1.amazonaws.com/prod/"
+
+BASE_URL="https://5xrmdxem70.execute-api.eu-west-1.amazonaws.com/prod"
 API_KEY="Xnh2MNqDoety1sXPXpOA30g5B1Hq5uU5LV4lpa5h"
 echo "Passing file: $FILE to POST"
 echo "File content:"
@@ -9,6 +10,7 @@ echo
 cat $FILE
 echo
 
+set -x
 curl -i -X POST -H "x-api-key: ${API_KEY}" -H "Content-Type: application/json" --data @${FILE} ${BASE_URL}/api/integration/maintenance-tracking
 
 
