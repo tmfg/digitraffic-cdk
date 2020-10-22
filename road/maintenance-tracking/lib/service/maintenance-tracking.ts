c989a7667d6a2bc7e5db2f781340ae5b5b465efe
Jouni Sonninen <jouni.sonninen@solita.fi>
2020-10-22 23:06:56 +0200

    DPO-1154 HARJA työkoneen seurantatiedon vastaanoton korvaaminen lambdalla - removed node-forge and use crypto

--- road/maintenance-tracking/lib/service/maintenance-tracking.ts
+++ road/maintenance-tracking/lib/service/maintenance-tracking.ts
@@ -2,10 +2,10 @@ import * as MaintenanceTrackingDB from "../db/db-maintenance-tracking"
 import {DbMaintenanceTrackingData, Status} from "../db/db-maintenance-tracking"
 import {inDatabase} from "../../../../common/postgres/database";
 import {IDatabase} from "pg-promise";
-import forge from "node-forge";
+
+const crypto = require('crypto');
 
 const matchViestitunnisteRegex = /"viestintunniste"\s*:\s*{\s*"id"\s*:\s*[0-9]*\s*}\s*,/;
-
 export async function saveMaintenanceTrackingData(maintenanceTrackingDataJson: string): Promise<any | undefined> {
     return await inDatabase(async (db: IDatabase<any, any>) => {
         const dbMaintenanceTrackingData: DbMaintenanceTrackingData = {
@@ -17,13 +17,11 @@ export async function saveMaintenanceTrackingData(maintenanceTrackingDataJson: s
             return await MaintenanceTrackingDB.insertMaintenanceTrackingData(db, dbMaintenanceTrackingData);
         });
     });
+
 }
 
 export function createHash(maintenanceTrackingDataJson: string) : string {
     // Strip away viestitunniste that changes between messages. We are interested only from the data around it.
     const data = maintenanceTrackingDataJson.replace(matchViestitunnisteRegex, '');
-    const md = forge.md.sha256.create();
-    md.update(data, 'utf8');
-    const hash = md.digest().toHex();
-    return hash;
+    return crypto.createHash("sha256").update(data).digest("hex");
 }
