c0271783a5aa3d8bc356aba0e09c71d36b3e70ed
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-10-23 11:50:03 +0300

    DPO-1039 uppercase locodes

--- marine/portcall-estimates/lib/subscriptions/db/db-subscriptions.ts
+++ marine/portcall-estimates/lib/subscriptions/db/db-subscriptions.ts
@@ -90,7 +90,7 @@ export async function removeSubscription(
         TableName: SUBSCRIPTIONS_TABLE_NAME,
         Key: {
             PhoneNumber: phoneNumber,
-            Locode: locode
+            Locode: locode.toUpperCase()
         }
     }).promise();
 }
