50fc9ca9d4b5e3b84c5a572cbdf9bcab9c5d29c5
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-11-09 09:49:53 +0200

    DPO-1062 update deps

--- update-deps.sh
+++ update-deps.sh
@@ -1,31 +1,20 @@
 #!/usr/bin/env bash
 
 # exit on any error
-set -e
-
-if ! npm list --depth 0 --global | grep -q npm-check-updates; then
-  echo "npm-check-updates not found, installing.."
-  npm install -g npm-check-updates
-  echo "..done"
-fi
+set -ex
 
 function update() {
   for d in $(find ./* -maxdepth 0 -type d); do
-    if [ "$d" != "./common" ] && [ "$d" != "./elasticsearch" ]; then
+    if [ "$d" != "./elasticsearch" ]; then
       cd "$d"
       rm -f package-lock.json
       rm -rf node_modules
-      ncu -u --reject pg-promise
       npm install
       npm run build
       cd ..
     fi
   done
 }
-
-cd common
-update
-cd ..
 
 cd road
 update
