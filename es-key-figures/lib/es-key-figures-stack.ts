076fa3ca19ac4ce4eee5ebfbcf783de866bbc558
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2020-11-27 12:53:14 +0200

    DPO-1180

--- es-key-figures/lib/es-key-figures-stack.ts
+++ es-key-figures/lib/es-key-figures-stack.ts
@@ -1,14 +1,69 @@
 import * as cdk from '@aws-cdk/core';
+import {Duration} from '@aws-cdk/core';
 import * as rds from '@aws-cdk/aws-rds';
 import {CfnDBClusterProps} from '@aws-cdk/aws-rds';
 import * as secretsmanager from '@aws-cdk/aws-secretsmanager'
 import * as ssm from '@aws-cdk/aws-ssm';
+import * as iam from '@aws-cdk/aws-iam';
+import * as logs from '@aws-cdk/aws-logs';
+import * as lambda from '@aws-cdk/aws-lambda';
 
+export interface Props {
+  elasticSearchEndpoint: string;
+  elasticSearchDomainArn: string;
+}
 
 export class EsKeyFiguresStack extends cdk.Stack {
-  constructor(app: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(app: cdk.App, id: string, esKeyFiguresProps: Props, props?: cdk.StackProps) {
     super(app, id, props);
     this.createDatabase(id);
+
+    const lambdaRole = new iam.Role(this, "CollectEsKeyFiguresRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      roleName: "CollectEsKeyFiguresRole"
+    });
+    lambdaRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: [
+          "es:DescribeElasticsearchDomain",
+          "es:DescribeElasticsearchDomains",
+          "es:DescribeElasticsearchDomainConfig",
+          "es:ESHttpPost",
+          "es:ESHttpPut"
+        ],
+        resources: [
+          esKeyFiguresProps.elasticSearchDomainArn,
+          `${esKeyFiguresProps.elasticSearchDomainArn}/*`
+        ]
+      })
+    );
+    lambdaRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:CreateLogGroup",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ],
+        resources: ["*"]
+      })
+    );
+
+    let functionName = 'CollectEsKeyFigures';
+    const lambdaConf = {
+      role: lambdaRole,
+      functionName: functionName,
+      code: new lambda.AssetCode('dist/lambda'),
+      handler: 'collect-es-key-figures.handler',
+      runtime: lambda.Runtime.NODEJS_10_X,
+      timeout: Duration.seconds(10),
+      logRetention: logs.RetentionDays.ONE_YEAR,
+      environment: {
+        ES_ENDPOINT: esKeyFiguresProps.elasticSearchEndpoint
+      }
+    };
+    const kinesisToESLambda = new lambda.Function(this, functionName, lambdaConf);
   }
 
   private createDatabase(id: string): void {
---
+++ es-key-figures/lib/lambda/collect-es-key-figures.ts
@@ -0,0 +1,5 @@
+import {Context} from "aws-lambda";
+
+export const handler = (event: any, context: Context, callback: any): void => {
+  console.info('test666')
+}
