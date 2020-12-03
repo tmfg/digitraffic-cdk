c76be70977f5ff9d44ccd162deb8e58acd25cde5
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-12-03 09:28:58 +0200

    DPO-1249 ditch curses

--- run-sam-local.py
+++ run-sam-local.py
@@ -1,15 +1,17 @@
-import os, yaml, sys
-from cursesmenu import *
-from cursesmenu.items import *
+import os, yaml
+from collections import namedtuple
 
-lambdas = {}
+Lambda = namedtuple("Lambda", "path lambdaname")
+
+lambdas = []
 
 def read_templates(paths):
     for path in paths:
         for f in os.listdir(path):
             joined_path = os.path.join(path, f)
             if os.path.isdir(joined_path) and os.path.isfile(os.path.join(joined_path, 'template.yaml')):
-                lambdas[joined_path] = read_lambdas(joined_path)
+                pass
+                lambdas.extend(read_lambdas(joined_path))
 
 def read_lambdas(dir):
     dir_lambdas = []
@@ -17,15 +19,20 @@ def read_lambdas(dir):
         cf = yaml.safe_load(f)
         for key, value in cf['Resources'].items():
             if value['Type'] == 'AWS::Lambda::Function' and 'LogRetention' not in key :
-                dir_lambdas.append(key)
+                dir_lambdas.append(Lambda(path=dir, lambdaname=key))
     return dir_lambdas
 
 read_templates(['marine', 'road'])
 
-menu = CursesMenu("Run a Lambda with SAM")
+i = 0
+for l in lambdas:
+    i = i+1
+    print '{}. {} {}'.format(i, l.path, l.lambdaname)
 
-for key, value in lambdas.items():
-    for lambda_name in value:
-        menu.append_item(CommandItem(key + ' - ' + lambda_name,  'cd {}; sam local invoke {} -d 9999; cd ..'.format(key, lambda_name), should_exit=True))
-
-menu.show()
+lambda_run_idx = int(raw_input())
+lambda_run_actual_idx = lambda_run_idx - 1
+if len(lambdas) - 1 < lambda_run_actual_idx:
+    print 'Lambda with number {} not found'.format(lambda_run_idx)
+else:
+    lambda_to_run = lambdas[lambda_run_actual_idx]
+    os.system('cd {}; sam local invoke {} ;'.format(lambda_to_run.path, lambda_to_run.lambdaname))
