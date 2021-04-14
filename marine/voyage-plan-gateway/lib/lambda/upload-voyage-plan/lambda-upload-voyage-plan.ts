c88f06c1b7e81418a0bf3ec1b399c38902fb993b
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-04-14 09:50:17 +0300

    DPO-1349 Validate incoming voyage plan XML

--- marine/voyage-plan-gateway/lib/lambda/upload-voyage-plan/lambda-upload-voyage-plan.ts
+++ marine/voyage-plan-gateway/lib/lambda/upload-voyage-plan/lambda-upload-voyage-plan.ts
@@ -1,9 +1,9 @@
 import * as util from 'util';
 import * as xml2js from 'xml2js';
-import {RtzVoyagePlan} from "../../../../../common/vis/voyageplan";
-import {BAD_REQUEST_MESSAGE} from "../../../../../common/api/errors";
+import {BAD_REQUEST_MESSAGE, OK_MESSAGE} from "../../../../../common/api/errors";
 import {withSecret} from "../../../../../common/secrets/secret";
-
+const libXml = require('libxmljs2-xsd');
+const rtzSchema = require('./rtz');
 
 /**
  * Implementation for the Sea Traffic Management (STM) Voyage Information Service (VIS) uploadVoyagePlan interface.
@@ -28,19 +28,26 @@ const secretId = process.env[KEY_SECRET_ID] as string;
 
 export function handlerFn(
     doWithSecret: (secretId: string, fn: (secret: any) => any) => any
-): (event: UploadVoyagePlanEvent) => Promise<void> {
-    return async function(event: UploadVoyagePlanEvent): Promise<void> {
+): (event: UploadVoyagePlanEvent) => Promise<string> {
+    return async function(event: UploadVoyagePlanEvent): Promise<string> {
         return await doWithSecret(secretId, async () => {
-            let voyagePlan: RtzVoyagePlan;
             try {
+                const schema = libXml.parse(rtzSchema);
+                const validationErrors = schema.validate(event.voyagePlan);
+                if (validationErrors) {
+                    console.log('Got XML validation errors', validationErrors);
+                    return Promise.reject(BAD_REQUEST_MESSAGE);
+                }
+
                 const parseXml = util.promisify(xml2js.parseString);
-                voyagePlan = (await parseXml(event.voyagePlan)) as RtzVoyagePlan;
+                // discard result for now, just make sure it parses ok
+                await parseXml(event.voyagePlan);
             } catch (error) {
                 console.error('UploadVoyagePlan XML parsing failed', error);
                 return Promise.reject(BAD_REQUEST_MESSAGE);
             }
             // do nothing currently
-            return Promise.resolve();
+            return JSON.stringify({message: OK_MESSAGE});
         });
     };
 }
