a5adb8d96676f92b1ac00392d65e3fc7e9952c01
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-04-15 14:19:56 +0300

    Remove syntax error

--- marine/portactivity/database/create-user.sql
+++ marine/portactivity/database/create-user.sql
@@ -8,7 +8,6 @@ GRANT SELECT ON public.vessel TO portactivity;
 GRANT SELECT ON public.port_call TO portactivity;
 GRANT SELECT ON public.port_area_details TO portactivity;
 
-# for tests
 GRANT INSERT,UPDATE,DELETE ON public.vessel TO portactivity;
 GRANT INSERT,UPDATE,DELETE ON public.port_call TO portactivity;
 GRANT INSERT,UPDATE,DELETE ON public.port_area_details TO portactivity;
