d02b99896976169b2eabbdd8faccf8f498d30506
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-04-21 12:27:56 +0300

    Remove portcall-estimates

--- marine/portcall-estimates/lib/subscriptions/service/sns.ts
+++
@@ -1,8 +0,0 @@
-import {SNS} from 'aws-sdk';
-
-export async function sendEmail(message: string): Promise<any> {
-    return new SNS().publish({
-        Message: message,
-        TopicArn: process.env.SHIPLIST_SNS_TOPIC_ARN
-    }).promise();
-}
