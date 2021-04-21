bda82859d063c0472a1284cbd5182cf530be22c6
Jouni Sonninen <jouni.sonninen@solita.fi>
2021-04-21 12:55:46 +0300

    DPO-1154 HARJA työkoneen seurantatiedon vastaanoton korvaaminen lambdalla - havainnon erotus

--- road/maintenance-tracking/lib/db/db-maintenance-tracking.ts
+++ road/maintenance-tracking/lib/db/db-maintenance-tracking.ts
@@ -39,3 +39,60 @@ export function createInsertMessageValues(e: DbMaintenanceTrackingData): any[] {
         e.sendingTime
     ];
 }
+
+
+export interface DbObservationData {
+    readonly id?: bigint,
+    readonly observationTime: Date,
+    readonly sendingTime: Date,
+    readonly json: string,
+    readonly harjaWorkmachineId: number,
+    readonly harjaContractId: number,
+    readonly status: Status,
+    readonly hash: string,
+    readonly s3Uri: string;
+}
+
+const UPSERT_MAINTENANCE_TRACKING_OBSERVATION_DATA_SQL = `
+    INSERT INTO MAINTENANCE_TRACKING_OBSERVATION_DATA(
+        id, 
+        observation_time,
+        sending_time,
+        json,
+        harja_workmachine_id,
+        harja_contract_id,
+        status,
+        hash,
+        s3_uri)
+    VALUES(
+        NEXTVAL('SEQ_MAINTENANCE_TRACKING_OBSERVATION_DATA'),
+        $(observationTime),
+        $(sendingTime),
+        $(json),
+        $(harjaWorkmachineId),
+        $(harjaContractId),
+        $(status),
+        $(hash),
+        $(s3Uri))
+    ON CONFLICT(hash) DO NOTHING
+`;
+
+export function insertMaintenanceTrackingObservationData(db: IDatabase<any, any>, observations: DbObservationData[]): Promise<any>[] {
+    return observations.map(observation => {
+        return db.none(UPSERT_MAINTENANCE_TRACKING_OBSERVATION_DATA_SQL,
+                       observation);
+    });
+}
+
+export function createInsertObservationValues(e: DbObservationData): any[] {
+    return [
+        e.observationTime,
+        e.sendingTime,
+        e.json,
+        e.harjaWorkmachineId,
+        e.harjaContractId,
+        e.status,
+        e.hash,
+        e.hash
+    ];
+}
