0a1eda5ace7e22fc17d129ba71cdb73a06aaa154
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-05-06 14:57:24 +0300

    DPO-1062 Update deps, remove some unused deps

--- update-deps.sh
+++ update-deps.sh
@@ -5,37 +5,4 @@
 # exit on any error
 set -ex
 
-function updateInDirectory() {
-    cd "$1"
-
-    if [ -f package.json ]; then
-      rm -rf node_modules
-
-      npm install
-      npm update
-      npm run build
-    fi
-
-    cd ..
-}
-
-function updateAllInDirectory() {
-  cd "$1"
-
-  for d in $(find ./* -maxdepth 0 -type d); do
-    updateInDirectory $d
-  done
-
-  cd ..
-}
-
-updateInDirectory common
-updateInDirectory cloudfront
-updateInDirectory elasticsearch
-updateInDirectory es-key-figures
-updateInDirectory user-management
-updateInDirectory status
-updateInDirectory swagger-joiner
-
-updateAllInDirectory road
-updateAllInDirectory marine
+yarn upgrade
