b351882e220bf8f589b50ea1cb783b303cd9c2a5
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-05-21 15:55:24 +0300

    DPO-1349 SNS FIFO has no Lambda trigger, replace it with SQS FIFO

--- marine/voyage-plan-gateway/lib/keys.ts
+++ marine/voyage-plan-gateway/lib/keys.ts
@@ -1,6 +1,7 @@
 export enum VoyagePlanEnvKeys {
     SECRET_ID = 'SECRET_ID',
-    TOPIC_ARN = 'TOPIC_ARN'
+    TOPIC_ARN = 'TOPIC_ARN',
+    QUEUE_URL = 'QUEUE_URL'
 }
 
 export enum VoyagePlanSecretKeys {
