e65d1a070e8584c105d4911d2b052bd36005ed82
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-06-02 07:37:37 +0300

    Don't allow query parameter usage in Swagger

--- swagger-joiner/lib/swagger-utils.ts
+++ swagger-joiner/lib/swagger-utils.ts
@@ -2,24 +2,33 @@ import {mergeDeepLeft} from "ramda";
 
 export function constructSwagger(spec: object) {
     return `
-        window.onload = function() {
-            const ui = SwaggerUIBundle({
-            spec: ${JSON.stringify(spec)},
-            dom_id: '#swagger-ui',
-            deepLinking: true,
-            docExpansion: 'none',
-            presets: [
-                SwaggerUIBundle.presets.apis,
-                SwaggerUIStandalonePreset.slice(1) // remove top bar plugin
-            ],
-            plugins: [
-                SwaggerUIBundle.plugins.DownloadUrl
-            ],
-            layout: "StandaloneLayout"
-            })
-            // End Swagger UI call region
-    
-            window.ui = ui
+        function showNotSupportedContent() {
+            document.querySelector('html').innerHTML = '<div>Query parameters not supported</div>';
+        }
+
+        const params = new URLSearchParams(window.location.search);
+        if (params.get('url') || params.get('spec') || params.get('urls') || params.get('configUrl')) {
+            showNotSupportedContent();
+        } else {
+            window.onload = function() {
+                const ui = SwaggerUIBundle({
+                spec: ${JSON.stringify(spec)},
+                dom_id: '#swagger-ui',
+                deepLinking: true,
+                docExpansion: 'none',
+                presets: [
+                    SwaggerUIBundle.presets.apis,
+                    SwaggerUIStandalonePreset.slice(1) // remove top bar plugin
+                ],
+                plugins: [
+                    SwaggerUIBundle.plugins.DownloadUrl
+                ],
+                layout: "StandaloneLayout"
+                })
+                // End Swagger UI call region
+        
+                window.ui = ui
+            }
         }
         `;
 }
