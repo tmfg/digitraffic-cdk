a62e99073ee0741aa94488dc1fcca2fad8aa29e0
Teijo Rönkä <teijo.ronka@solita.fi>
2021-06-16 12:35:06 +0300

    flyway command parameterized

--- marine/portactivity/database/update-db.sh
+++ marine/portactivity/database/update-db.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 ENVIRONMENT=${1:-dev}
+COMMAND=${2:-migrate}
 
 docker run --rm \
 --name portactivity-db-updater \
@@ -8,4 +9,4 @@ docker run --rm \
 -v $(pwd)/update:/flyway/sql \
 --network=dnet1 \
 flyway/flyway \
-migrate
+$COMMAND
