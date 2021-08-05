91393b32cf267c92830dea930287d64b5e0e3442
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-08-05 13:51:29 +0300

    DPO-1480 Use secrets, send area traffic from lambda

--- marine/shiplight/lib/lambda/update-lights/lambda-update-lights.ts
+++ marine/shiplight/lib/lambda/update-lights/lambda-update-lights.ts
@@ -1,6 +1,23 @@
 import * as AreaTrafficService from '../../service/areatraffic';
+import {ShiplightEnvKeys} from "../../keys";
+import {withSecret} from "digitraffic-common/secrets/secret";
+import {ShiplightSecret} from "../../model/shiplight-secret";
+import {updateLightsForArea} from "../../api/arealights";
+import {AreaTraffic} from "../../model/areatraffic";
 
-export async function handler() {
-    const areas = await AreaTrafficService.getAreaTraffic();
-    // TODO send
+const secretId = process.env[ShiplightEnvKeys.SECRET_ID] as string;
+
+async function handlerFn(
+    doWithSecret: (secretId: string, fn: (secret: any) => any) => any,
+    updateLightsForAreaFn: (area: AreaTraffic, endpointUrl: string) => any
+) {
+    return await doWithSecret(secretId, async (secret: ShiplightSecret) => {
+        const areas = await AreaTrafficService.getAreaTraffic();
+
+        for (const area of areas) {
+            await updateLightsForAreaFn(area, secret.endpointUrl);
+        }
+    });
 }
+
+export const handler = handlerFn(withSecret, updateLightsForArea);
---
+++ marine/shiplight/lib/model/shiplight-secret.ts
@@ -0,0 +1,3 @@
+export type ShiplightSecret = {
+    readonly endpointUrl: string
+}
