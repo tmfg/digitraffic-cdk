49b19389a989fc464b7e48a319d3b76e45d284fd
Teijo Rönkä <teijo.ronka@solita.fi>
2021-08-12 13:59:17 +0300

    DPO-1476 one more test, more comments

--- marine/shiplight/test/db/areatraffic.test.ts
+++ marine/shiplight/test/db/areatraffic.test.ts
@@ -12,7 +12,7 @@ describe('db-areatraffic', dbTestBase((db: IDatabase<any, any>) => {
     test('getAreaTraffic - one-hit', async () => {
         await insertAreaTraffic(db, 1, 'testi1', 10, "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))");
         await insertVessel(db, 1, 70); // 70 will trigger
-        await insertVesselLocation(db, 1, Date.now(), 1);
+        await insertVesselLocation(db, 1, Date.now(), 1); // x = 1, in the polygon
 
         const traffic = await getAreaTraffic(db);
 
@@ -22,7 +22,7 @@ describe('db-areatraffic', dbTestBase((db: IDatabase<any, any>) => {
     test('getAreaTraffic - wrong ship-type', async () => {
         await insertAreaTraffic(db, 1, 'testi1', 10, "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))");
         await insertVessel(db, 1, 1); // ship_type 1 will not trigger
-        await insertVesselLocation(db, 1, Date.now(), 1);
+        await insertVesselLocation(db, 1, Date.now(), 1); // x = 1, in the polygon
 
         const traffic = await getAreaTraffic(db);
 
@@ -32,12 +32,22 @@ describe('db-areatraffic', dbTestBase((db: IDatabase<any, any>) => {
     test('getAreaTraffic - not in the area', async () => {
         await insertAreaTraffic(db, 1, 'testi1', 10, "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))");
         await insertVessel(db, 1, 70); // 70 will trigger
-        await insertVesselLocation(db, 1, Date.now(), -1);
+        await insertVesselLocation(db, 1, Date.now(), -1); // x = -1, not in the polygon
 
         const traffic = await getAreaTraffic(db);
 
         expect(traffic).toHaveLength(0);
     });
 
+    test('getAreaTraffic - timestamp not in window', async () => {
+        await insertAreaTraffic(db, 1, 'testi1', 10, "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))");
+        await insertVessel(db, 1, 70); // 70 will trigger
+        // timestamp is not in the 2 minute window
+        await insertVesselLocation(db, 1, Date.now() - 2*60*1000, -1); // x = 1, in the polygon
+
+        const traffic = await getAreaTraffic(db);
+
+        expect(traffic).toHaveLength(0);
+    });
 
 }));
