6dc8698285096838a928dc92bf5c4d0d76ab2f7c
Teijo Rönkä <teijo.ronka@solita.fi>
2021-08-12 13:27:59 +0300

    DPO-1476 more tests

--- marine/shiplight/test/db/areatraffic.test.ts
+++ marine/shiplight/test/db/areatraffic.test.ts
@@ -11,11 +11,33 @@ describe('db-areatraffic', dbTestBase((db: IDatabase<any, any>) => {
 
     test('getAreaTraffic - one-hit', async () => {
         await insertAreaTraffic(db, 1, 'testi1', 10, "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))");
-        await insertVessel(db, 1, 70);
+        await insertVessel(db, 1, 70); // 70 will trigger
         await insertVesselLocation(db, 1, Date.now(), 1);
 
         const traffic = await getAreaTraffic(db);
 
         expect(traffic).toHaveLength(1);
     });
+
+    test('getAreaTraffic - wrong ship-type', async () => {
+        await insertAreaTraffic(db, 1, 'testi1', 10, "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))");
+        await insertVessel(db, 1, 1); // ship_type 1 will not trigger
+        await insertVesselLocation(db, 1, Date.now(), 1);
+
+        const traffic = await getAreaTraffic(db);
+
+        expect(traffic).toHaveLength(0);
+    });
+
+    test('getAreaTraffic - not in the area', async () => {
+        await insertAreaTraffic(db, 1, 'testi1', 10, "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))");
+        await insertVessel(db, 1, 70); // 70 will trigger
+        await insertVesselLocation(db, 1, Date.now(), -1);
+
+        const traffic = await getAreaTraffic(db);
+
+        expect(traffic).toHaveLength(0);
+    });
+
+
 }));
