2b877e9b2c08fd0337498d986c65764df7d44526
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-08-12 12:51:13 +0300

    DPO-1288 Remove extra keys

--- other/status/lib/keys.ts
+++ other/status/lib/keys.ts
@@ -3,10 +3,3 @@ export enum StatusEnvKeys {
     APPS = 'APPS',
     CHECK_TIMEOUT_SECONDS = 'CHECK_TIMEOUT_SECONDS'
 }
-
-export enum StatusSecretKeys {
-    SCHEDULES_ACCESS_TOKEN = 'vpgw.schedulesAccessToken',
-    SCHEDULES_URL = 'vpgw.schedulesUrl',
-    PUBLIC_VIS_URL = 'vpgw.publicVisUrl',
-    HMAC = 'vpgw.hmac'
-}
