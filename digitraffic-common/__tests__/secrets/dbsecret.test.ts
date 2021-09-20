95ee2407aa829680190ac3aa05795cd34d21e12d
Teijo Rönkä <teijo.ronka@solita.fi>
2021-09-20 15:01:08 +0300

    DPO-1494 more tests

---
+++ digitraffic-common/__tests__/secrets/dbsecret.test.ts
@@ -0,0 +1,57 @@
+import {mockSecret, stubSecretsManager} from "../../test/secrets-manager";
+
+import * as sinon from 'sinon';
+
+const SECRET_ID = "test_secret";
+const SECRET_WITH_PREFIX = {
+    "prefix.value" : "value",
+    "prefix.name" : "name",
+    "wrong.value" : "value"
+}
+const SECRET_EMPTY = {};
+
+stubSecretsManager();
+
+import {getSecret, withSecret, withSecretAndPrefix} from "../../secrets/secret";
+
+describe('secret - test', () => {
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    test('getSecret - no secret', async () => {
+        mockSecret(null);
+
+        await expect(async () => {
+            await withSecret(SECRET_ID, () => {});
+        }).rejects.toThrowError("No secret found!");
+    });
+
+    test('getSecret - empty secret', async () => {
+        mockSecret(SECRET_EMPTY);
+
+        await withSecret(SECRET_ID, (secret: any) => {
+            expect(secret).toEqual(SECRET_EMPTY);
+        });
+    });
+
+    test('getSecret - no prefix', async () => {
+        mockSecret(SECRET_WITH_PREFIX);
+
+        await withSecret(SECRET_ID, (secret: any) => {
+            expect(secret).toEqual(SECRET_WITH_PREFIX);
+        });
+    });
+
+    test('getSecret - with prefix', async () => {
+        mockSecret(SECRET_WITH_PREFIX);
+
+        await withSecretAndPrefix(SECRET_ID, 'prefix', (secret: any) => {
+            expect(secret).toEqual({
+                value: "value",
+                name: "name"
+            });
+        });
+           
+    });
+});
