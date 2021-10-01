2c3e1ef1e7a815a3d38d71798f00c2fc7e0ae52b
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-10-01 09:22:54 +0300

    DPO-1552 Add configuration possibilities

--- digitraffic-common/lambda/monitoredfunction.ts
+++ digitraffic-common/lambda/monitoredfunction.ts
@@ -2,21 +2,38 @@ import {Function, FunctionProps} from '@aws-cdk/aws-lambda';
 import {Stack} from "@aws-cdk/core";
 import {ITopic} from "@aws-cdk/aws-sns";
 import {SnsAction} from "@aws-cdk/aws-cloudwatch-actions";
-import {ComparisonOperator} from "@aws-cdk/aws-cloudwatch";
+import {ComparisonOperator, Metric} from "@aws-cdk/aws-cloudwatch";
 import {TrafficType} from '../model/traffictype';
 
-export enum MonitoredFunctionAlarm {
-    DURATION,
-    ERRORS,
-    THROTTLES,
-    DURATION_WARNING
+/**
+ * Allows customization of CloudWatch Alarm properties
+ */
+export type MonitoredFunctionAlarmProps = {
+    /**
+     * Setting this to false will not create a CloudWatch alarm
+     */
+    readonly create: boolean
+
+    readonly threshold?: number
+
+    readonly evaluationPeriods?: number
+
+    readonly datapointsToAlarm?: number
+
+    readonly statistic?: string
+
+    readonly comparisonOperator?: ComparisonOperator
 }
 
 export type MonitoredFunctionProps = {
-    /**
-     *  Use to create alarms only for certain metrics
-     */
-    readonly includeAlarms?: MonitoredFunctionAlarm[]
+
+    readonly durationAlarmProps?: MonitoredFunctionAlarmProps
+
+    readonly durationWarningProps?: MonitoredFunctionAlarmProps
+
+    readonly errorAlarmProps?: MonitoredFunctionAlarmProps
+
+    readonly throttleAlarmProps?: MonitoredFunctionAlarmProps
 }
 
 /**
@@ -47,55 +64,100 @@ export class MonitoredFunction extends Function {
         const alarmSnsAction = new SnsAction(alarmSnsTopic);
         const warningSnsAction = new SnsAction(warningSnsTopic);
 
-        if (!functionProps.timeout) {
-            throw new Error('Timeout needs to be explicitly set');
+        if (props?.durationAlarmProps?.create !== false) {
+            if (!functionProps.timeout) {
+                throw new Error('Timeout needs to be explicitly set');
+            }
+            this.createAlarm(scope,
+                this.metricDuration(),
+                'Duration',
+                'Duration alarm',
+                `Duration has exceeded ${functionProps.timeout!.toSeconds()} seconds`,
+                trafficType,
+                alarmSnsAction,
+                functionProps.timeout!.toMilliseconds(),
+                1,
+                1,
+                'max',
+                ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+                props?.durationAlarmProps);
         }
-        if (!props || props.includeAlarms?.includes(MonitoredFunctionAlarm.DURATION)) {
-            this.metricDuration().createAlarm(scope, `${this.node.id}-Duration`, {
-                alarmName: `${trafficType ?? ''} ${scope.stackName} ${this.functionName} duration alarm`.trim(),
-                alarmDescription: `${this.functionName} duration has exceeded ${functionProps.timeout!.toSeconds()} seconds`,
-                threshold: functionProps.timeout!.toMilliseconds(),
-                evaluationPeriods: 1,
-                datapointsToAlarm: 1,
-                statistic: 'max',
-                comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD
-            }).addAlarmAction(alarmSnsAction);
-        }
-        if (!props || props.includeAlarms?.includes(MonitoredFunctionAlarm.DURATION_WARNING)) {
-            this.metricDuration().createAlarm(scope, `${this.node.id}-Duration-Warning`, {
-                alarmName: `${trafficType ?? ''} ${scope.stackName} ${this.functionName} duration warning`.trim(),
-                alarmDescription: `${this.functionName} duration is 85 % of max ${functionProps.timeout!.toSeconds()} seconds`,
-                threshold: functionProps.timeout!.toMilliseconds() * 0.85,
-                evaluationPeriods: 1,
-                datapointsToAlarm: 1,
-                statistic: 'max',
-                comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD
-            }).addAlarmAction(warningSnsAction);
+        if (props?.durationWarningProps?.create !== false) {
+            if (!functionProps.timeout) {
+                throw new Error('Timeout needs to be explicitly set');
+            }
+            this.createAlarm(scope,
+                this.metricDuration(),
+                'Duration-Warning',
+                'Duration warning',
+                `Duration is 85 % of max ${functionProps.timeout!.toSeconds()} seconds`,
+                trafficType,
+                warningSnsAction,
+                functionProps.timeout!.toMilliseconds() * 0.85,
+                1,
+                1,
+                'max',
+                ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+                props?.durationWarningProps);
         }
 
-        if (!props || props.includeAlarms?.includes(MonitoredFunctionAlarm.ERRORS)) {
-            this.metricErrors().createAlarm(scope, `${this.node.id}-Errors`, {
-                alarmName: `${trafficType ?? ''} ${scope.stackName} ${this.functionName} errors alarm`.trim(),
-                alarmDescription: `${this.functionName} invocations did not succeed`,
-                threshold: 1,
-                evaluationPeriods: 1,
-                datapointsToAlarm: 1,
-                statistic: 'sum',
-                comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD
-            }).addAlarmAction(alarmSnsAction);
+        if (props?.errorAlarmProps?.create !== false) {
+            this.createAlarm(scope,
+                this.metricErrors(),
+                'Errors',
+                'Errors alarm',
+                'Invocations did not succeed',
+                trafficType,
+                alarmSnsAction,
+                1,
+                1,
+                1,
+                'sum',
+                ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+                props?.errorAlarmProps);
         }
 
-        if (!props || props.includeAlarms?.includes(MonitoredFunctionAlarm.THROTTLES)) {
-            this.metricThrottles().createAlarm(scope, `${this.node.id}-Throttles`, {
-                alarmName: `${trafficType ?? ''} ${scope.stackName} ${this.functionName} throttles alarm`.trim(),
-                alarmDescription: `${this.functionName} has throttled`,
-                threshold: 0,
-                evaluationPeriods: 1,
-                datapointsToAlarm: 1,
-                statistic: 'sum',
-                comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD
-            }).addAlarmAction(alarmSnsAction);
+        if (props?.throttleAlarmProps?.create !== false) {
+            this.createAlarm(scope,
+                this.metricThrottles(),
+                'Throttles',
+                'Throttles alarm',
+                'Has throttled',
+                trafficType,
+                alarmSnsAction,
+                0,
+                1,
+                1,
+                'sum',
+                ComparisonOperator.GREATER_THAN_THRESHOLD,
+                props?.throttleAlarmProps);
         }
+    }
+
+    private createAlarm(
+        scope: Stack,
+        metric: Metric,
+        alarmId: string,
+        alarmName: string,
+        alarmDescription: string,
+        trafficType: TrafficType | null,
+        alarmSnsAction: SnsAction,
+        threshold: number,
+        evaluationPeriods: number,
+        datapointsToAlarm: number,
+        statistic: string,
+        comparisonOperator: ComparisonOperator,
+        alarmProps?: MonitoredFunctionAlarmProps
+    ) {
+        metric.createAlarm(scope, `${this.node.id}-${alarmId}`, {
+            alarmName: `${trafficType ?? ''} ${scope.stackName} ${this.functionName} ${alarmName}`.trim(),
+            alarmDescription,
+            threshold: alarmProps?.threshold ?? threshold,
+            evaluationPeriods: alarmProps?.evaluationPeriods ?? evaluationPeriods,
+            datapointsToAlarm: alarmProps?.datapointsToAlarm ?? datapointsToAlarm,
+            statistic: alarmProps?.statistic ?? statistic,
+            comparisonOperator: alarmProps?.comparisonOperator ?? comparisonOperator
+        }).addAlarmAction(alarmSnsAction);
     }
 
 }
