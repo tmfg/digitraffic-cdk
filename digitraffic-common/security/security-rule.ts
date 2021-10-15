f0f27c624b3cc70170efe43fe598c2db9ecb6f78
Teijo Rönkä <teijo.ronka@solita.fi>
2021-10-15 08:05:30 +0300

    add comment for rule, add compliance failed

--- digitraffic-common/security/security-rule.ts
+++ digitraffic-common/security/security-rule.ts
@@ -3,6 +3,12 @@ import {Rule} from "@aws-cdk/aws-events";
 import {ITopic} from "@aws-cdk/aws-sns";
 import {SnsTopic} from "@aws-cdk/aws-events-targets";
 
+/**
+ * Automatic rule for Security Hub.  Send notification to given topic if the following conditions apply:
+ * * There is a finding with a status of NEW
+ * * It has severity of HIGH or CRITICAL
+ * * It is in a FAILED state
+ */
 export class DigitrafficSecurityRule extends Rule {
     constructor(scope: Construct, topic: ITopic) {
         const ruleName = 'SecurityHubRule';
@@ -13,6 +19,9 @@ export class DigitrafficSecurityRule extends Rule {
                 detailType: ["Security Hub Findings - Imported"],
                 detail: {
                     findings: {
+                        "Compliance": {
+                            "Status": ["FAILED"]
+                        },
                         "Workflow": {
                             "Status": ["NEW"]
                         },
