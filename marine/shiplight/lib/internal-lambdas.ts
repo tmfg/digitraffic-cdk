b646962b28494307cfbbc0840a549082ec2d87c8
Teijo Rönkä <teijo.ronka@solita.fi>
2021-10-15 08:05:02 +0300

    use common scheduler

--- marine/shiplight/lib/internal-lambdas.ts
+++ marine/shiplight/lib/internal-lambdas.ts
@@ -1,32 +1,17 @@
 import {AssetCode, Function} from '@aws-cdk/aws-lambda';
-import {Stack} from '@aws-cdk/core';
-import {dbFunctionProps, dbLambdaConfiguration} from 'digitraffic-common/stack/lambda-configs';
+import {dbFunctionProps} from 'digitraffic-common/stack/lambda-configs';
 import {createSubscription} from 'digitraffic-common/stack/subscription';
-import {Rule, Schedule} from "@aws-cdk/aws-events";
-import {LambdaFunction} from "@aws-cdk/aws-events-targets";
 import {ISecret} from "@aws-cdk/aws-secretsmanager";
-import {ShiplightEnvKeys} from "./keys";
-import {LambdaEnvironment} from "digitraffic-common/model/lambda-environment";
-import {DatabaseEnvironmentKeys} from "digitraffic-common/secrets/dbsecret";
 import {DigitrafficStack} from "digitraffic-common/stack/stack";
 import {TrafficType} from "digitraffic-common/model/traffictype";
 import {MonitoredFunction} from "digitraffic-common/lambda/monitoredfunction";
+import {Scheduler} from "digitraffic-common/scheduler/scheduler";
 
 export function create(
     secret: ISecret,
     stack: DigitrafficStack) {
 
-    const updateLightsLambda = createUpdateLightsLambda(secret, stack);
-    const schedulingRule = createScheduler(stack);
-    schedulingRule.addTarget(new LambdaFunction(updateLightsLambda));
-}
-
-function createScheduler(stack: Stack): Rule {
-    const ruleName = 'Shiplight-Scheduler'
-    return new Rule(stack, ruleName, {
-        ruleName,
-        schedule: Schedule.expression('cron(*/1 * * * ? *)') // every 1 minutes
-    });
+    Scheduler.everyMinute(stack, 'Shiplight-Scheduler', createUpdateLightsLambda(secret, stack));
 }
 
 function createUpdateLightsLambda(
