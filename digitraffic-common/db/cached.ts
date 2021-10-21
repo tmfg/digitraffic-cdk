cac101ea714d56d2373bb8d5d095a79f2d518dc9
Teijo Rönkä <teijo.ronka@solita.fi>
2021-10-21 09:22:34 +0300

    DPO-1559 use prepared statements

--- digitraffic-common/db/cached.ts
+++ digitraffic-common/db/cached.ts
@@ -1,4 +1,4 @@
-import {IDatabase} from "pg-promise";
+import {IDatabase, PreparedStatement} from "pg-promise";
 
 const SQL_UPDATE_CACHE_VALUE =
     `insert into cached_json(cache_id, content, last_updated)
@@ -10,15 +10,25 @@ const SQL_GET_CACHE_VALUE =
     `select content, last_updated from cached_json
     where cache_id = $1`;
 
+const PS_UPDATE_CACHE_VALUE = new PreparedStatement({
+    name: 'update-cache-value',
+    text: SQL_UPDATE_CACHE_VALUE
+})
+
+const PS_GET_CACHE_VALUE = new PreparedStatement({
+    name: 'get-cache-value',
+    text: SQL_GET_CACHE_VALUE
+})
+
 export enum JSON_CACHE_KEY {
     NAUTICAL_WARNINGS_ACTIVE = 'nautical-warnings-active',
     NAUTICAL_WARNINGS_ARCHIVED = 'nautical-warnings-archived'
 }
 
 export function updateCachedJson(db: IDatabase<any, any>, cacheKey: JSON_CACHE_KEY, value: any): Promise<any> {
-    return db.none(SQL_UPDATE_CACHE_VALUE, [cacheKey, value]);
+    return db.none(PS_UPDATE_CACHE_VALUE, [cacheKey, value]);
 }
 
 export function getJsonFromCache(db: IDatabase<any, any>, cacheKey: JSON_CACHE_KEY): Promise<any> {
-    return db.oneOrNone(SQL_GET_CACHE_VALUE, [cacheKey]).then(value => value?.content ?? null);
+    return db.oneOrNone(PS_GET_CACHE_VALUE, [cacheKey]).then(value => value?.content ?? null);
 }
