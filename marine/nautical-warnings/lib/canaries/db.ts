467d257606497c4a2443eba10f7b55f293c3e466
Teijo Rönkä <teijo.ronka@solita.fi>
2021-10-21 08:23:33 +0300

    DPO-1559 fix keys

--- marine/nautical-warnings/lib/canaries/db.ts
+++ marine/nautical-warnings/lib/canaries/db.ts
@@ -1,6 +1,6 @@
 import {DatabaseChecker} from "digitraffic-common/canaries/database-checker";
 import {SECRET_ID} from "digitraffic-common/model/lambda-environment";
-import {CACHE_KEY_ACTIVE, CACHE_KEY_ARCHIVED} from "../service/nautical-warnings";
+import {JSON_CACHE_KEY} from "digitraffic-common/db/cached";
 
 const secretId = process.env[SECRET_ID] as string;
 
@@ -8,17 +8,17 @@ export const handler = async () => {
     const checker = new DatabaseChecker(secretId);
 
     checker.one('active cache is not empty',
-        `select count(*) from cached_json where cache_id = '${CACHE_KEY_ACTIVE}'`);
+        `select count(*) from cached_json where cache_id = '${JSON_CACHE_KEY.NAUTICAL_WARNINGS_ACTIVE}'`);
 
     checker.one('archived cache is not empty',
-        `select count(*) from cached_json where cache_id = '${CACHE_KEY_ARCHIVED}'`);
+        `select count(*) from cached_json where cache_id = '${JSON_CACHE_KEY.NAUTICAL_WARNINGS_ARCHIVED}'`);
 
     checker.one('active cache updated in last 15 minutes',
-        `select count(*) from cached_json where cache_id = '${CACHE_KEY_ARCHIVED}'
+        `select count(*) from cached_json where cache_id = '${JSON_CACHE_KEY.NAUTICAL_WARNINGS_ACTIVE}'
             and last_updated > now() - interval '15 minutes'`);
 
     checker.one('archived cache updated in last 15 minutes',
-        `select count(*) from cached_json where cache_id = '${CACHE_KEY_ARCHIVED}'
+        `select count(*) from cached_json where cache_id = '${JSON_CACHE_KEY.NAUTICAL_WARNINGS_ARCHIVED}'
             and last_updated > now() - interval '15 minutes'`);
 
     return checker.expect();
