57fbfc942df6a590ec95a4a7a3c5c986dd514dba
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-10-25 17:46:03 +0300

    DPO-1575 Refactor

--- marine/portactivity/lib/service/awake_ai_atx.ts
+++ marine/portactivity/lib/service/awake_ai_atx.ts
@@ -17,44 +17,46 @@ export class AwakeAiATXService {
 
     async getATXs(ports: Port[], timeoutMillis: number): Promise<ApiTimestamp[]> {
         const atxs = await this.api.getATXs(timeoutMillis);
-
         return inDatabase(async (db: IDatabase<any, any>) => {
-
-            const ret: ApiTimestamp[] = [];
-
-            for (const atx of atxs.filter(atx => atx.locodes.some(locode => ports.includes(locode)))) {
-                // pick the first supported LOCODE
-                const port =  atx.locodes.find(locode => ports.includes(locode))!;
-                const eventType = atx.zoneEventType == AwakeATXZoneEventType.ARRIVAL ? EventType.ATA : EventType.ATD;
-                const eventTime = moment(atx.eventTimestamp).toDate();
-                const portcallId = await TimestampDAO.findPortcallId(db,
-                    port,
-                    eventType,
-                    eventTime,
-                    atx.mmsi,
-                    atx.imo);
-
-                if (portcallId) {
-                    ret.push({
+            const promises = atxs.filter(atx => atx.locodes.some(locode => ports.includes(locode)))
+                .map(async (atx) => {
+                    // pick the first supported LOCODE
+                    const port =  atx.locodes.find(locode => ports.includes(locode))!;
+                    const eventType = atx.zoneEventType == AwakeATXZoneEventType.ARRIVAL ? EventType.ATA : EventType.ATD;
+                    const eventTime = moment(atx.eventTimestamp).toDate();
+                    const portcallId = await TimestampDAO.findPortcallId(db,
+                        port,
                         eventType,
-                        eventTime: atx.eventTimestamp,
-                        source: EventSource.AWAKE_AI,
-                        recordTime: atx.eventTimestamp,
-                        ship: {
-                            imo: atx.imo,
-                            mmsi: atx.mmsi
-                        },
-                        location: {
-                            port
-                        },
-                        portcallId
-                    });
-                } else {
-                    console.warn('method=getATXs no portcall found for %s IMO', atx.zoneEventType, atx.imo);
-                }
-            }
+                        eventTime,
+                        atx.mmsi,
+                        atx.imo);
 
-            return ret;
+                    if (portcallId) {
+                        return {
+                            timestamp: {
+                                eventType,
+                                eventTime: atx.eventTimestamp,
+                                source: EventSource.AWAKE_AI,
+                                recordTime: atx.eventTimestamp,
+                                ship: {
+                                    imo: atx.imo,
+                                    mmsi: atx.mmsi
+                                },
+                                location: {
+                                    port
+                                },
+                                portcallId
+                            }
+                        };
+                    } else {
+                        console.warn('method=getATXs no portcall found for %s IMO', atx.zoneEventType, atx.imo);
+                        return {
+                            timestamp: null
+                        };
+                    }
+                });
+            return Promise.all(promises)
+                .then(atxs => atxs.filter(atx => !!atx.timestamp).map(atx => atx.timestamp));
         });
     }
 }
