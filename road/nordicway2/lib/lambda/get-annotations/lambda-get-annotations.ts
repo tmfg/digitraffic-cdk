6bca024ff9c1fbf454283e983d18cca379dbf640
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-10-27 06:49:19 +0300

    Remove nordicway2

--- road/nordicway2/lib/lambda/get-annotations/lambda-get-annotations.ts
+++
@@ -1,9 +0,0 @@
-import * as AnnotationsService from "../../service/annotations";
-
-export const handler = async (event: any): Promise<any> => {
-    const author = event['author'] as string | null;
-    const type = event['type'] as string | null;
-
-    return await AnnotationsService.findActiveAnnotations(author, type);
-};
-
