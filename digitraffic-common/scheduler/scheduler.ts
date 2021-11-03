7df5424c289f963e7251a5f067870994d09e7651
Teijo Rönkä <teijo.ronka@solita.fi>
2021-11-03 08:12:24 +0200

    marinecam concurrency up

--- digitraffic-common/scheduler/scheduler.ts
+++ digitraffic-common/scheduler/scheduler.ts
@@ -13,14 +13,18 @@ export class Scheduler extends Rule {
     }
 
     static everyMinute(stack: Construct, ruleName: string, lambda?: Function) {
-        return new Scheduler(stack, ruleName, Schedule.rate(Duration.minutes(1)), lambda);
+        return Scheduler.every(stack, ruleName, Duration.minutes(1), lambda);
     }
 
     static everyMinutes(stack: Construct, ruleName: string, minutes: number, lambda?: Function) {
-        return new Scheduler(stack, ruleName, Schedule.rate(Duration.minutes(minutes)), lambda);
+        return Scheduler.every(stack, ruleName, Duration.minutes(minutes), lambda);
     }
 
     static everyHour(stack: Construct, ruleName: string, lambda?: Function) {
-        return new Scheduler(stack, ruleName, Schedule.rate(Duration.hours(1)), lambda);
+        return Scheduler.every(stack, ruleName, Duration.hours(1), lambda);
+    }
+
+    static every(stack: Construct, ruleName: string, duration: Duration, lambda?: Function) {
+        return new Scheduler(stack, ruleName, Schedule.rate(duration), lambda);
     }
 }
--- marine/marinecam/lib/internal-lambdas.ts
+++ marine/marinecam/lib/internal-lambdas.ts
@@ -1,11 +1,10 @@
-import {Rule, Schedule} from '@aws-cdk/aws-events';
-import {LambdaFunction} from '@aws-cdk/aws-events-targets';
 import {Bucket} from "@aws-cdk/aws-s3";
 import {DigitrafficLogSubscriptions} from "digitraffic-common/stack/subscription";
 import {MarinecamEnvKeys} from "./keys";
 import {DigitrafficStack} from "digitraffic-common/stack/stack";
 import {MonitoredFunction} from "digitraffic-common/lambda/monitoredfunction";
 import {MobileServerProps} from "./app-props";
+import {Scheduler} from "digitraffic-common/scheduler/scheduler";
 
 export function create(
     stack: DigitrafficStack,
@@ -24,12 +23,11 @@ function createUpdateImagesLambda(stack: DigitrafficStack,
     const environment = stack.createLambdaEnvironment();
     environment[MarinecamEnvKeys.BUCKET_NAME] = bucket.bucketName;
 
-    const lambda = MonitoredFunction.createV2(stack, 'update-images', environment);
-
-    const rule = new Rule(stack, 'Rule', {
-        schedule: Schedule.rate((stack.configuration as MobileServerProps).updateFrequency)
+    const lambda = MonitoredFunction.createV2(stack, 'update-images', environment, {
+        reservedConcurrentExecutions: 2
     });
-    rule.addTarget(new LambdaFunction(lambda));
+
+    Scheduler.every(stack, 'UpdateImages-Rule', (stack.configuration as MobileServerProps).updateFrequency, lambda);
 
     return lambda;
 }
