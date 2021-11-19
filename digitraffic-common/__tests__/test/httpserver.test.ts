099ff6c0e0db817b602b9dc055e17bd578500a1a
Teijo Rönkä <teijo.ronka@solita.fi>
2021-11-19 10:34:59 +0200

    fix test

--- digitraffic-common/__tests__/test/httpserver.test.ts
+++ digitraffic-common/__tests__/test/httpserver.test.ts
@@ -37,6 +37,10 @@ describe('TestHttpServer - test', () => {
                 port: PORT,
                 method
             }, (response: IncomingMessage) => {
+                //another chunk of data has been received, so append it to `str`
+                response.on('data', (chunk: string) => {
+                    // do nothing
+                });
 
                 //the whole response has been received, so we just print it out here
                 response.on('end', () => {
