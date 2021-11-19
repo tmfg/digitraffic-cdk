d5a9920e8f6d30908cedefdb1b15e5545519f414
Teijo Rönkä <teijo.ronka@solita.fi>
2021-11-19 11:07:31 +0200

    DPO-1616 use actual functions and function.toString()

--- digitraffic-common/queue/sqs.ts
+++ digitraffic-common/queue/sqs.ts
@@ -9,6 +9,8 @@ import {RetentionDays} from "@aws-cdk/aws-logs";
 import {SqsEventSource} from "@aws-cdk/aws-lambda-event-sources";
 import {ComparisonOperator, TreatMissingData} from "@aws-cdk/aws-cloudwatch";
 import {SnsAction} from "@aws-cdk/aws-cloudwatch-actions";
+import {S3} from "aws-sdk";
+import {ManagedUpload} from "aws-sdk/clients/s3";
 
 /**
  * Construct for creating SQS-queues.
@@ -90,13 +92,15 @@ function addDLQAlarm(stack: DigitrafficStack, dlqName: string, dlq: Queue) {
 }
 
 function getDlqCode(bucketName: string): InlineCode {
-    return new InlineCode(DLQ_LAMBDA_CODE.replace('_bucketName_', bucketName));
+    const functionBody = DLQ_LAMBDA_CODE
+        .replace('_bucketName_',bucketName)
+        .replace("__upload__", uploadToS3.toString())
+        .replace("__doUpload__", doUpload.toString());
+
+    return new InlineCode(functionBody);
 }
 
-const DLQ_LAMBDA_CODE = `const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-
-async function uploadToS3(bucketName, body, objectName) {
+async function uploadToS3(s3: S3, bucketName: string, body: string, objectName: string): Promise<void> {
     try {
         console.info('writing %s to %s', objectName, bucketName);
         await doUpload(s3, bucketName, body, objectName);
@@ -111,18 +115,21 @@ async function uploadToS3(bucketName, body, objectName) {
     }
 }
 
-function doUpload(s3, bucketName, body, filename) {
+function doUpload(s3: S3, Bucket: string, Body: string, Key: string): Promise<ManagedUpload.SendData> {
     return s3.upload({
-        Bucket: bucketName,
-        Body: body,
-        Key: filename
+        Bucket, Body, Key
     }).promise();
 }
+
+const DLQ_LAMBDA_CODE = `const AWS = require('aws-sdk');
+
+__upload__
+__doUpload__
 
 exports.handler = async (event) => {
     const millis = new Date().getTime();
     return Promise.all(event.Records.map((e, idx) =>
-        uploadToS3('_bucketName_', e.body, \`dlq-$\{millis\}-$\{idx\}.json\`)
+        uploadToS3(new AWS.S3(), '_bucketName_', e.body, \`dlq-$\{millis\}-$\{idx\}.json\`)
     ));
 };
 `;
