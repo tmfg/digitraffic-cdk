b148c6e8649d6ef2ea2772c6a492a8b5e2f965b6
Teijo Rönkä <teijo.ronka@solita.fi>
2021-11-24 15:12:07 +0200

    added await

--- marine/marinecam/lib/lambda/get-metadata/get-metadata.ts
+++ marine/marinecam/lib/lambda/get-metadata/get-metadata.ts
@@ -2,14 +2,13 @@ import * as MetadataService from '../../service/metadata';
 
 import {withDbSecret} from "digitraffic-common/secrets/dbsecret";
 import {SECRET_ID} from "digitraffic-common/model/lambda-environment";
-import {Camera} from "../../model/camera";
 import {LambdaResponse} from "digitraffic-common/lambda/lambda-response";
 
 const secretId = process.env[SECRET_ID] as string;
 
 export const handler = async () : Promise<LambdaResponse> => {
     try {
-        const cameras = withDbSecret(secretId, async (): Promise<Camera[]> => {
+        const cameras = await withDbSecret(secretId, () => {
             return MetadataService.listAllCameras(['Saimaa']);
         });
 
