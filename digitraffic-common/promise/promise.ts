658beee017b075381bfb20713a3c94ac8fafa4ae
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-11-24 14:17:27 +0200

    Utility function for retrying promises

--- digitraffic-common/promise/promise.ts
+++ digitraffic-common/promise/promise.ts
@@ -1,3 +1,22 @@
-export type NullablePromiseResult<T> = {
-    value: T | null
+/**
+ * Utility function for retrying async functions.
+ * @param asyncFn Function
+ * @param retries Amount of retries. If set to <= 0, no retries will be done.
+ * @param logError If the promise rejects, should the error be logged?
+ */
+export async function retry(asyncFn: () => Promise<void>, retries: number, logError: boolean): Promise<void> {
+    try {
+        await asyncFn();
+    } catch (error) {
+        if (logError) {
+            console.error('method=retry error', error);
+        }
+        const remainingRetries = retries - 1;
+        if (remainingRetries <= 0) {
+            console.warn('method=retry no retries left');
+            return;
+        }
+        console.warn('method=retry invocation failed, retrying with remaining retries %d', remainingRetries);
+        return retry(asyncFn, remainingRetries, logError);
+    }
 }
