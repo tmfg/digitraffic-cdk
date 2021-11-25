5b2c73e66e1535b1a27df0d7531ef4eb294272e4
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-11-25 09:37:04 +0200

    Add support for retry return value

--- digitraffic-common/__tests__/promise/promise.test.ts
+++ digitraffic-common/__tests__/promise/promise.test.ts
@@ -4,10 +4,11 @@ import {getRandomInteger} from "../../test/testutils";
 describe('Promise utils tests', () => {
 
     test('retry - no retries', async () => {
-        const fn = jest.fn();
+        const fn = jest.fn().mockResolvedValue(1);
 
-        await retry(fn, 0, false);
+        const ret = await retry(fn, 0, false);
 
+        expect(ret).toBe(1);
         expect(fn.mock.calls.length).toBe(1);
     });
 
@@ -17,15 +18,16 @@ describe('Promise utils tests', () => {
 
         await retry(fn, retries, false);
 
-        expect(fn.mock.calls.length).toBe(retries);
+        expect(fn.mock.calls.length).toBe(retries + 1);
     });
 
     test('retry - no error with n+1 retries', async () => {
-        const fn = jest.fn();
+        const fn = jest.fn().mockResolvedValue(1);
         const retries = getRandomInteger(1, 10);
 
-        await retry(fn, retries, false);
+        const ret = await retry(fn, retries, false);
 
+        expect(ret).toBe(1);
         expect(fn.mock.calls.length).toBe(1);
     });
 
@@ -56,7 +58,7 @@ describe('Promise utils tests', () => {
 
         await retry(fn, retries, true);
 
-        expect(consoleErrorSpy).toHaveBeenCalledTimes(retries);
+        expect(consoleErrorSpy).toHaveBeenCalledTimes(retries + 1);
         consoleErrorSpy.mockRestore();
     });
 
@@ -66,14 +68,50 @@ describe('Promise utils tests', () => {
 
         await retry(fn);
 
-        expect(fn.mock.calls.length).toBe(3);
+        expect(fn.mock.calls.length).toBe(3 + 1);
         expect(consoleErrorSpy).toHaveBeenCalledTimes(0);
     });
 
     test('retry - NaN throws error', async () => {
-        const fn = jest.fn().mockRejectedValue('aa');
+        const fn = jest.fn();
 
         await expect(() => retry(fn, NaN, false)).rejects.toThrow();
     });
 
+    test('retry - Infinity throws error', async () => {
+        const fn = jest.fn();
+
+        await expect(() => retry(fn, Infinity, false)).rejects.toThrow();
+    });
+
+    test('retry - exceeded maximum retry count throws error', async () => {
+        const fn = jest.fn();
+
+        await expect(() => retry(fn, getRandomInteger(101, 1000000), false)).rejects.toThrow();
+    });
+
+    test('retry - use without mocks without retry', async () => {
+        const val = 1;
+        const fn = async () => val;
+
+        const ret = await retry(fn);
+
+        expect(ret).toBe(val);
+    });
+
+    test('retry - use without mocks with retry', async () => {
+        let i = 0;
+        const val = 1;
+        const fn = async () => {
+            if (i < 3) {
+                i++;
+                throw new Error('not yet');
+            }
+            return val;
+        };
+
+        const ret = await retry(fn);
+
+        expect(ret).toBe(val);
+    });
 });
--- digitraffic-common/promise/promise.ts
+++ digitraffic-common/promise/promise.ts
@@ -1,23 +1,26 @@
 /**
  * Utility function for retrying async functions.
  * @param asyncFn Function
- * @param retries Amount of retries, default is 3. If set to <= 0, no retries will be done. Using NaN will throw an error.
+ * @param retries Amount of retries, default is 3. If set to <= 0, no retries will be done. Using non-finite numbers will throw an error. The maximum allowed retry count is 100.
  * @param logError If the promise rejects, should the error be logged? Default is false.
  */
-export async function retry(asyncFn: () => Promise<void>, retries = 3, logError = false): Promise<void> {
-    if (isNaN(retries)) {
-        throw new Error('NaN is not supported');
+export async function retry<T>(asyncFn: () => Promise<T>, retries = 3, logError = false): Promise<T | null> {
+    if (!isFinite(retries)) {
+        throw new Error('Only finite numbers are supported');
+    }
+    if (retries > 100) {
+        throw new Error('Exceeded the maximum retry count of 100');
     }
     try {
-        await asyncFn();
+        return await asyncFn();
     } catch (error) {
         if (logError) {
             console.error('method=retry error', error);
         }
         const remainingRetries = retries - 1;
-        if (remainingRetries <= 0) {
+        if (remainingRetries < 0) {
             console.warn('method=retry no retries left');
-            return;
+            return null;
         }
         console.warn('method=retry invocation failed, retrying with remaining retries %d', remainingRetries);
         return retry(asyncFn, remainingRetries, logError);
