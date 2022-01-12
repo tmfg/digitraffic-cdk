6a3a7fa24f932127f6dee066cac29a465bc667d6
Teijo Rönkä <teijo.ronka@solita.fi>
2022-01-12 07:14:08 +0200

    DPO-1612 deprecations

--- digitraffic-common/aws/types/tags.ts
+++ digitraffic-common/aws/types/tags.ts
@@ -1,4 +1,3 @@
-// DEPRECATED! REMOVE THIS
+// DEPRECATED! This must be replaced with new application specific tags
 export const BETA_TAGS = ['Beta'];
 export const DATA_V1_TAGS = ['Data v1'];
-export const USER_MANAGEMENT_TAGS = ['User Management'];
