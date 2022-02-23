981305f7f7520929f9a7b15f306cbb7d6489e40a
Teijo Rönkä <teijo.ronka@solita.fi>
2022-02-23 07:21:55 +0200

    DPO-1708 review fixes

--- marine/marinecam/lib/service/image-store.ts
+++ marine/marinecam/lib/service/image-store.ts
@@ -1,23 +1,21 @@
 import {S3} from "aws-sdk";
 import {MediaType} from "digitraffic-common/aws/types/mediatypes";
 import {ManagedUpload} from "aws-sdk/lib/s3/managed_upload";
+import {writeFile} from 'fs/promises';
 
 const BASE64 = 'base64';
 
-export function storeImage(cameraId: string, image: string, bucketName: string): Promise<ManagedUpload.SendData> {
+export function storeImage(cameraId: string, image: string, bucketName: string) {
     const imageName = `${cameraId}.jpg`;
 
     console.info("storing image %s to s3 with size %d", cameraId, image.length);
 
     // for local testing
     if (bucketName === '') {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const fs = require('fs');
-
         console.info("storing image %s locally!", cameraId);
         // store to s3
 
-        return fs.writeFileSync(imageName, image, BASE64);
+        return writeFile(imageName, image, BASE64);
     } else {
         const keyName = `images/Saimaa/${imageName}`;
 
