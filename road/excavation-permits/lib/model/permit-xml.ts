b66fa0aa30dd1fb9cb47a0d0b3690745f6fc322c
Ilari Junnonen <ilari.junnonen@solita.fi>
2022-02-28 16:02:02 +0200

    DPO-1739 Review fixes

---
+++ road/excavation-permits/lib/model/permit-xml.ts
@@ -0,0 +1,18 @@
+export interface PermitResponse {
+    readonly "wfs:FeatureCollection": {
+        readonly "gml:featureMember": PermitElement[]
+    };
+}
+
+export interface PermitElement {
+    readonly "GIS:YlAlLuvat": {
+        readonly "GIS:Id": string,
+        readonly "GIS:Lupatyyppi": string,
+        readonly "GIS:VoimassaolonAlkamispaiva": string,
+        readonly "GIS:VoimassaolonAlkamisaika": string,
+        readonly "GIS:VoimassaolonPaattymispaiva": string,
+        readonly "GIS:VoimassaolonPaattymissaika": string,
+        readonly "GIS:LuvanTarkoitus": string,
+        readonly "GIS:Geometry": Record<string, unknown>
+    }
+}
