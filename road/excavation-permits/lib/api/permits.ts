b3962b21b9d6b1dd38fbe929f9ad56e8989dd5c4
Ilari Junnonen <ilari.junnonen@solita.fi>
2022-02-28 16:02:02 +0200

    DPO-1739 Review fixes

--- road/excavation-permits/lib/api/permits.ts
+++ road/excavation-permits/lib/api/permits.ts
@@ -1,16 +1,26 @@
 import axios from 'axios';
 
-export async function getXml(apiUrl: string,
-    path: string,
-    authKey: string): Promise<string> {
-    const resp = await axios.get(`${apiUrl}${path}?authKey=${authKey}`, {
-        headers: {
-            'Accept': 'application/xml',
-        },
-    });
-    if (resp.status !== 200) {
-        console.error(`method=getXml query to ${path} failed status ${resp.status}`);
-        throw Error(`Query to ${path} failed`);
+export class PermitsApi {
+    readonly apiUrl: string;
+    readonly path: string;
+    readonly authKey: string;
+
+    constructor() {
+        this.apiUrl = "https://lahti.infraweb.fi:1880";
+        this.path = "/api/v1/kartat/luvat/voimassa";
+        this.authKey = process.env.AUTH_KEY as string;
+    };
+
+    async getPermitsXml(): Promise<string> {
+        const resp = await axios.get(`${this.apiUrl}${this.path}?authKey=${this.authKey}`, {
+            headers: {
+                'Accept': 'application/xml',
+            },
+        });
+        if (resp.status !== 200) {
+            console.error(`method=getXml query to ${this.path} failed status ${resp.status}`);
+            throw Error(`Query to ${this.path} failed`);
+        }
+        return resp.data;
     }
-    return resp.data;
 }
