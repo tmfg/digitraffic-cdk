8b31da779ce83cb2d9f00b18bb569afb29763008
Ilari Junnonen <ilari.junnonen@solita.fi>
2022-03-04 12:53:56 +0200

    DPO-1739 Review fixes

--- road/excavation-permits/lib/api/permits.ts
+++ road/excavation-permits/lib/api/permits.ts
@@ -12,15 +12,19 @@ export class PermitsApi {
     }
 
     async getPermitsXml(): Promise<string> {
-        const resp = await axios.get(`${this.apiUrl}${this.path}?authKey=${this.authKey}`, {
-            headers: {
-                'Accept': 'application/xml',
-            },
-        });
-        if (resp.status !== 200) {
-            console.error(`method=getXml query to ${this.path} failed status ${resp.status}`);
-            throw Error(`Query to ${this.path} failed`);
+        try {
+            const resp = await axios.get(`${this.apiUrl}${this.path}?authKey=${this.authKey}`, {
+                headers: {
+                    'Accept': 'application/xml',
+                },
+            });
+            if (resp.status !== 200) {
+                console.error(`method=getXml query to ${this.path} failed status ${resp.status}`);
+                throw Error(`Query to ${this.path} failed`);
+            }
+            return resp.data;
+        } catch (error) {
+            throw error;
         }
-        return resp.data;
     }
 }
