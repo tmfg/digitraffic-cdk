77d79065319c2646105e591b6833cb4f72893dea
Jani Haakana <jani.k.haakana@solita.fi>
2022-04-28 12:12:49 +0300

    DPO-1685 Change Proxy event to RestApi event

--- road/road-network-conditions/lib/lambda/get-alarms/get-alarms.ts
+++ road/road-network-conditions/lib/lambda/get-alarms/get-alarms.ts
@@ -1,13 +1,14 @@
-import {ProxyLambdaRequest, ProxyLambdaResponse} from "digitraffic-common/aws/types/proxytypes";
+import {ProxyLambdaResponse} from "digitraffic-common/aws/types/proxytypes";
 import {MediaType} from "digitraffic-common/aws/types/mediatypes";
 import {SecretHolder} from "digitraffic-common/aws/runtime/secrets/secret-holder";
 import * as rcs from "../../service/road-network-conditions-service";
 import {Alarms} from "../../model/alarms";
 import {DOMAIN_PREFIX, RoadNetworkConditionsSecret} from "../../model/road-network-conditions-secret";
+import {DigitrafficRestApi} from "digitraffic-common/aws/infra/stack/rest_apis";
 
 const holder = SecretHolder.create<RoadNetworkConditionsSecret>(DOMAIN_PREFIX);
 
-export async function handler(event: ProxyLambdaRequest): Promise<ProxyLambdaResponse> {
+export async function handler(event: DigitrafficRestApi): Promise<ProxyLambdaResponse> {
     const secret = await holder.get();
 
     const result: Alarms = await rcs.getAlarms(secret.apiKey, secret.url);
--- road/road-network-conditions/lib/lambda/get-devices-geojson/get-devices-geojson.ts
+++ road/road-network-conditions/lib/lambda/get-devices-geojson/get-devices-geojson.ts
@@ -1,13 +1,14 @@
-import {ProxyLambdaRequest, ProxyLambdaResponse} from "digitraffic-common/aws/types/proxytypes";
+import {ProxyLambdaResponse} from "digitraffic-common/aws/types/proxytypes";
 import {MediaType} from "digitraffic-common/aws/types/mediatypes";
 import {SecretHolder} from "digitraffic-common/aws/runtime/secrets/secret-holder";
 import * as rcs from "../../service/road-network-conditions-service";
 import {DOMAIN_PREFIX, RoadNetworkConditionsSecret} from "../../model/road-network-conditions-secret";
 import {FeatureCollection} from "geojson";
+import {DigitrafficRestApi} from "digitraffic-common/aws/infra/stack/rest_apis";
 
 const holder = SecretHolder.create<RoadNetworkConditionsSecret>(DOMAIN_PREFIX);
 
-export async function handler(event: ProxyLambdaRequest): Promise<ProxyLambdaResponse> {
+export async function handler(event: DigitrafficRestApi): Promise<ProxyLambdaResponse> {
     const secret = await holder.get();
 
     const result: FeatureCollection = await rcs.getDevicesGeojson(secret.apiKey, secret.url);
