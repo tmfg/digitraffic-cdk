52b3aa6acb8c38ec7d591b153d2d5e484219c184
Massimo Prencipe <massimo.prencipe@solita.fi>
2022-06-08 13:40:41 +0300

    DPO-1755 Add support for Swagger update roles

--- other/aws-github-oidc/lib/aws-github-oidc-stack.ts
+++ other/aws-github-oidc/lib/aws-github-oidc-stack.ts
@@ -1,16 +1,12 @@
 import {Duration, Stack, StackProps} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-import {
-    Effect,
-    ManagedPolicy,
-    OpenIdConnectProvider,
-    PolicyStatement,
-    Role,
-    WebIdentityPrincipal,
-} from "aws-cdk-lib/aws-iam";
+import {Effect, OpenIdConnectProvider, PolicyStatement, Role, WebIdentityPrincipal} from "aws-cdk-lib/aws-iam";
+import {Function} from 'aws-cdk-lib/aws-lambda';
+import {TrafficType} from "digitraffic-common/types/traffictype";
 
 export type AwsGithubProps = {
     readonly roles: {
+        readonly updateSwaggerForTrafficType?: TrafficType
         readonly shortName: string
         readonly repo: string
         readonly environment: string
@@ -28,25 +24,42 @@ export class AwsGithubOidcStack extends Stack {
             thumbprints: [
                 'a031c46782e6e6c662c2c87c76da9aa62ccabd8e',
                 '6938fd4d98bab03faadb97b34396831e3780aea1',
-            ]
+            ],
         });
 
         props.roles.forEach(role => {
-            new Role(this, `${role.shortName}-OIDCRole`, {
-                assumedBy: new WebIdentityPrincipal(
-                    `arn:aws:iam::${this.account}:oidc-provider/token.actions.githubusercontent.com`, {
-                        StringLike: {
-                            'token.actions.githubusercontent.com:sub': `repo:tmfg/${role.repo}:environment:${role.environment}`,
-                        },
-                    }),
+            const envCapitalized = role.environment.replace(/^\w/, (c) => c.toUpperCase());
+            const createdRole = new Role(this, `${role.shortName}-OIDCRole`, {
+                assumedBy: new WebIdentityPrincipal(`arn:aws:iam::${this.account}:oidc-provider/token.actions.githubusercontent.com`, {
+                    StringLike: {
+                        'token.actions.githubusercontent.com:sub': `repo:tmfg/${role.repo}:environment:${role.environment}`,
+                    },
+                }),
                 roleName: `${role.shortName}-GitHub-OIDC-Role`,
                 description: `Used by GitHub Actions to perform ${role.shortName} related actions in this account`,
                 maxSessionDuration: Duration.hours(1),
-            }).addToPrincipalPolicy(new PolicyStatement({
-                effect: Effect.ALLOW,
-                actions: role.allowedActions,
-                resources: ['*'],
-            }));
+            });
+            if (role.allowedActions.length) {
+                createdRole.addToPrincipalPolicy(new PolicyStatement({
+                    effect: Effect.ALLOW,
+                    actions: role.allowedActions,
+                    resources: ['*'],
+                }));
+            }
+            if (role.updateSwaggerForTrafficType) {
+                const updateApiDocsFunctionName = `SwaggerJoiner${role.updateSwaggerForTrafficType}${envCapitalized}-UpdateApiDocumentation`;
+                const updateSwaggerDocsFunctionName = `SwaggerJoiner${role.updateSwaggerForTrafficType}${envCapitalized}-UpdateSwaggerDescriptions`;
+                createdRole.addToPrincipalPolicy(new PolicyStatement({
+                    effect: Effect.ALLOW,
+                    actions: [
+                        'lambda:InvokeFunction',
+                    ],
+                    resources: [
+                        Function.fromFunctionName(this, updateApiDocsFunctionName, updateApiDocsFunctionName).functionArn,
+                        Function.fromFunctionName(this, updateSwaggerDocsFunctionName, updateSwaggerDocsFunctionName).functionArn,
+                    ],
+                }));
+            }
         });
     }
 }
