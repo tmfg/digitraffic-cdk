6a26a723ed5aa2958bd12e9994f623bdd3b4ca25
Ilari Junnonen <ilari.junnonen@solita.fi>
2022-07-18 16:49:04 +0300

    DPO-1829 Define digitraffic-statistics roles and security groups in stack

--- other/digitraffic-statistics/lib/digitraffic-statistics-stack.ts
+++ other/digitraffic-statistics/lib/digitraffic-statistics-stack.ts
@@ -1,24 +1,23 @@
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigw from 'aws-cdk-lib/aws-apigateway';
-import * as route53 from 'aws-cdk-lib/aws-route53';
-import * as targets from 'aws-cdk-lib/aws-route53-targets';
-import * as acm from 'aws-cdk-lib/aws-certificatemanager';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import {Effect, Role} from 'aws-cdk-lib/aws-iam';
-import {SecurityGroup, Vpc} from 'aws-cdk-lib/aws-ec2';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as targets from "aws-cdk-lib/aws-route53-targets";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
+import {Peer, Port, SecurityGroup, Vpc} from "aws-cdk-lib/aws-ec2";
 import {Construct} from "constructs";
 import {Duration} from "aws-cdk-lib";
-import cdk = require('aws-cdk-lib');
+import cdk = require("aws-cdk-lib");
 
-const path = require('path');
+const path = require("path");
 
 interface StatisticsProps {
     readonly vpcName: string;
-    readonly sgName: string;
-    readonly roleArn: string;
+    readonly dbSecretArn: string;
     readonly certificateArn: string,
     readonly visualizationsBucketArn: string,
+    readonly rdsSgId: string,
     readonly allowedIpAddresses: string[],
     readonly figuresLambdaEnv: {
         readonly API_GATEWAY_BASE_PATH: string,
@@ -40,20 +39,30 @@ export class DigitrafficStatisticsStack extends cdk.Stack {
             vpcName: customProps.vpcName
         });
 
-        const sg = SecurityGroup.fromLookupByName(this, "digitraffic-monthly-sg", customProps.sgName, vpc);
+        const digitrafficMonthlySg = new SecurityGroup(this, "digitraffic-monthly-sg", {
+            vpc,
+            allowAllOutbound: true,
+            securityGroupName: "digitraffic-monthly-sg"
+        });
 
-        const digitrafficFiguresFunction = new lambda.DockerImageFunction(this, 'digitraffic-monthly', {
+        const RDSEsKeyFiguresSG = SecurityGroup.fromLookupById(this, "es-key-figures-sg", customProps.rdsSgId);
+        RDSEsKeyFiguresSG.addIngressRule(
+            Peer.securityGroupId(digitrafficMonthlySg.securityGroupId),
+            Port.tcp(3306)
+        );
+
+        const digitrafficMonthlyFunction = new lambda.DockerImageFunction(this, 'digitraffic-monthly', {
             functionName: "digitraffic-monthly",
             code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../digitraffic-figures/'), {}),
             vpc: vpc,
-            securityGroups: [sg],
-            role: Role.fromRoleArn(this, "digitraffic-figures-role", customProps.roleArn),
+            securityGroups: [digitrafficMonthlySg],
+            role: this.createDigitrafficMonthlyLambdaRole(customProps.dbSecretArn),
             architecture: lambda.Architecture.ARM_64,
             memorySize: 4096,
             timeout: Duration.seconds(60),
             environment: customProps.figuresLambdaEnv
         });
-        const digitrafficFiguresLambdaIntegration = new apigw.LambdaIntegration(digitrafficFiguresFunction, {
+        const digitrafficMonthlyLambdaIntegration = new apigw.LambdaIntegration(digitrafficMonthlyFunction, {
             proxy: true
         });
 
@@ -93,10 +102,10 @@ export class DigitrafficStatisticsStack extends cdk.Stack {
 
         const digitrafficMonthly = restApi.root.addResource("digitraffic-monthly",
             {
-                defaultIntegration: digitrafficFiguresLambdaIntegration,
+                defaultIntegration: digitrafficMonthlyLambdaIntegration,
             });
         digitrafficMonthly.addProxy({
-            defaultIntegration: digitrafficFiguresLambdaIntegration,
+            defaultIntegration: digitrafficMonthlyLambdaIntegration,
             anyMethod: true
         })
         digitrafficMonthly.addMethod("GET");
@@ -104,7 +113,7 @@ export class DigitrafficStatisticsStack extends cdk.Stack {
         const domain = "statistics.digitraffic.fi";
         const domainName = new apigw.DomainName(this, 'domain', {
             domainName: domain,
-            certificate: acm.Certificate.fromCertificateArn(this, "digitraffic-statistics-certificate", "arn:aws:acm:eu-west-1:370197853960:certificate/c382d868-fd6c-4d0c-970e-2eac98387a27"),
+            certificate: acm.Certificate.fromCertificateArn(this, "digitraffic-statistics-certificate", customProps.certificateArn),
             endpointType: apigw.EndpointType.REGIONAL
         });
         domainName.addBasePathMapping(restApi,{stage: restApi.deploymentStage});
@@ -125,13 +134,13 @@ export class DigitrafficStatisticsStack extends cdk.Stack {
         return new iam.PolicyDocument({
             statements: [
                 new iam.PolicyStatement({
-                    effect: Effect.ALLOW,
+                    effect: iam.Effect.ALLOW,
                     actions: ["execute-api:Invoke"],
                     principals: [new iam.AnyPrincipal()],
                     resources: ["*"]
                 }),
                 new iam.PolicyStatement({
-                    effect: Effect.DENY,
+                    effect: iam.Effect.DENY,
                     actions: ["execute-api:Invoke"],
                     conditions: {
                         "NotIpAddress": {
@@ -145,10 +154,53 @@ export class DigitrafficStatisticsStack extends cdk.Stack {
         })
     }
 
+    private createDigitrafficMonthlyLambdaRole(dbSecretArn: string) {
+        const lambdaRole = new iam.Role(this, "digitraffic-monthly-lambda-role", {
+            assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+            roleName: "digitraffic-monthly-lambda-role",
+        });
+        lambdaRole.addToPolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                    "ec2:CreateNetworkInterface",
+                    "logs:CreateLogStream",
+                    "ec2:DescribeNetworkInterfaces",
+                    "logs:DescribeLogGroups",
+                    "logs:DescribeLogStreams",
+                    "ec2:DeleteNetworkInterface",
+                    "logs:CreateLogGroup",
+                    "logs:PutLogEvents"
+                ],
+                resources: ["*"]
+            })
+        )
+        lambdaRole.addToPolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: ["secretsmanager:GetSecretValue"],
+                resources: [dbSecretArn]
+            })
+        )
+        lambdaRole.addToPolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                    "xray:PutTraceSegments",
+                    "xray:PutTelemetryRecords"
+                ],
+                resources: ["*"],
+            })
+        )
+        lambdaRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("CloudWatchLambdaInsightsExecutionRolePolicy"));
+
+        return lambdaRole;
+    }
+
     private createS3ExecutionRole(bucket: s3.IBucket) {
-        const executeRole = new iam.Role(this, "api-gateway-s3-assume-role", {
+        const executeRole = new iam.Role(this, "digitraffic-api-statistics-apigw-s3-integration-role", {
             assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
-            roleName: "digitraffic-statistics-apigw-s3-integration-role",
+            roleName: "digitraffic-api-statistics-apigw-s3-integration-role",
         });
         executeRole.addToPolicy(
             new iam.PolicyStatement({
@@ -159,5 +211,3 @@ export class DigitrafficStatisticsStack extends cdk.Stack {
         return executeRole;
     }
 }
-
-
