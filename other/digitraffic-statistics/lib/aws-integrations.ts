4deff10b4d90edc7848b5434c50aa94c0950a889
Ilari Junnonen <ilari.junnonen@solita.fi>
2022-08-25 09:54:52 +0300

    DPO-1829 Split statistics stack

---
+++ other/digitraffic-statistics/lib/aws-integrations.ts
@@ -0,0 +1,136 @@
+import {AwsIntegration, LambdaIntegration} from "aws-cdk-lib/aws-apigateway";
+import {Bucket} from "aws-cdk-lib/aws-s3";
+import {DigitrafficStatisticsStack} from "./digitraffic-statistics-stack";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import path from "path";
+import {Duration} from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
+import {IVpc, Peer, Port, SecurityGroup, Vpc} from "aws-cdk-lib/aws-ec2";
+
+export class StatisticsIntegrations {
+
+    readonly apiStatisticsS3Integration: AwsIntegration;
+    readonly digitrafficMonthlyLambdaIntegration: LambdaIntegration;
+
+    constructor(stack: DigitrafficStatisticsStack) {
+
+        const vpc = Vpc.fromLookup(stack, "EsKeyFiguresVPC", {
+            vpcName: stack.statisticsProps.vpcName
+        });
+
+        const digitrafficMonthlySg = new SecurityGroup(stack, "digitraffic-monthly-sg", {
+            vpc,
+            allowAllOutbound: true,
+            securityGroupName: "digitraffic-monthly-sg"
+        });
+
+        this.createDbAccessIngressRule(stack, digitrafficMonthlySg);
+
+        this.apiStatisticsS3Integration = this.createApiStatisticsS3Integration(stack);
+        this.digitrafficMonthlyLambdaIntegration = this.createDigitrafficMonthlyLambdaIntegration(stack, vpc, digitrafficMonthlySg);
+
+    }
+
+    private createDbAccessIngressRule(stack: DigitrafficStatisticsStack, sg: SecurityGroup) {
+        const rdsEsKeyFiguresSg = SecurityGroup.fromLookupById(stack, "es-key-figures-sg", stack.statisticsProps.rdsSgId);
+        rdsEsKeyFiguresSg.addIngressRule(
+            Peer.securityGroupId(sg.securityGroupId),
+            Port.tcp(3306)
+        );
+    }
+
+    private createDigitrafficMonthlyLambdaIntegration(stack: DigitrafficStatisticsStack, vpc: IVpc, sg: SecurityGroup): LambdaIntegration {
+        const digitrafficMonthlyFunction = new lambda.DockerImageFunction(stack, 'digitraffic-monthly', {
+            functionName: "digitraffic-monthly",
+            code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../digitraffic-figures/'), {}),
+            vpc: vpc,
+            securityGroups: [sg],
+            role: this.createDigitrafficMonthlyLambdaRole(stack),
+            architecture: lambda.Architecture.ARM_64,
+            memorySize: 4096,
+            timeout: Duration.seconds(60),
+            environment: stack.statisticsProps.figuresLambdaEnv
+        });
+
+        return new LambdaIntegration(digitrafficMonthlyFunction, {
+            proxy: true
+        });
+    }
+
+    private createApiStatisticsS3Integration(stack: DigitrafficStatisticsStack): AwsIntegration {
+        const apiStatisticsBucket = Bucket.fromBucketArn(stack, "api-statistics-bucket", stack.statisticsProps.visualizationsBucketArn);
+        return new AwsIntegration({
+            service: "s3",
+            integrationHttpMethod: "GET",
+            path: `${apiStatisticsBucket.bucketName}/index.html`,
+            options: {
+                credentialsRole: this.createS3ExecutionRole(stack, apiStatisticsBucket),
+                integrationResponses: [{
+                    statusCode: "200",
+                    responseParameters: {
+                        'method.response.header.Content-Type': "'text/html'",
+                    },
+                }]
+            }
+        })
+    }
+
+    private createS3ExecutionRole(stack: DigitrafficStatisticsStack, bucket: s3.IBucket): iam.Role {
+        const executeRole = new iam.Role(stack, "digitraffic-api-statistics-apigw-s3-integration-role", {
+            assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
+            roleName: "digitraffic-api-statistics-apigw-s3-integration-role",
+        });
+        executeRole.addToPolicy(
+            new iam.PolicyStatement({
+                resources: [`${bucket.bucketArn}/*`],
+                actions: ["s3:GetObject"],
+            })
+        );
+        return executeRole;
+    }
+
+    private createDigitrafficMonthlyLambdaRole(stack: DigitrafficStatisticsStack): iam.Role {
+        const lambdaRole = new iam.Role(stack, "digitraffic-monthly-lambda-role", {
+            assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+            roleName: "digitraffic-monthly-lambda-role",
+        });
+        lambdaRole.addToPolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                    "ec2:CreateNetworkInterface",
+                    "logs:CreateLogStream",
+                    "ec2:DescribeNetworkInterfaces",
+                    "logs:DescribeLogGroups",
+                    "logs:DescribeLogStreams",
+                    "ec2:DeleteNetworkInterface",
+                    "logs:CreateLogGroup",
+                    "logs:PutLogEvents"
+                ],
+                resources: ["*"]
+            })
+        )
+        lambdaRole.addToPolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: ["secretsmanager:GetSecretValue"],
+                resources: [stack.statisticsProps.dbSecretArn]
+            })
+        )
+        lambdaRole.addToPolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                    "xray:PutTraceSegments",
+                    "xray:PutTelemetryRecords"
+                ],
+                resources: ["*"],
+            })
+        )
+        lambdaRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("CloudWatchLambdaInsightsExecutionRolePolicy"));
+
+        return lambdaRole;
+    }
+
+}
--- other/digitraffic-statistics/lib/digitraffic-statistics-stack.ts
+++ other/digitraffic-statistics/lib/digitraffic-statistics-stack.ts
@@ -1,16 +1,11 @@
-import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as route53 from "aws-cdk-lib/aws-route53";
 import * as targets from "aws-cdk-lib/aws-route53-targets";
-import * as s3 from "aws-cdk-lib/aws-s3";
-import * as iam from "aws-cdk-lib/aws-iam";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
-import * as ec2 from "aws-cdk-lib/aws-ec2";
 import {Construct} from "constructs";
-import {Duration} from "aws-cdk-lib";
+import {StatisticsIntegrations} from "./aws-integrations";
+import {StatisticsApi} from "./api";
 import cdk = require("aws-cdk-lib");
-
-const path = require("path");
 
 interface DigitrafficStatisticsProps {
     readonly vpcName: string;
@@ -19,6 +14,7 @@ interface DigitrafficStatisticsProps {
     readonly visualizationsBucketArn: string,
     readonly rdsSgId: string,
     readonly allowedIpAddresses: string[],
+    readonly domainName: string,
     readonly figuresLambdaEnv: {
         readonly API_GATEWAY_BASE_PATH: string,
         readonly API_GATEWAY_STAGE_PATH: string,
@@ -32,182 +28,37 @@ interface DigitrafficStatisticsProps {
 
 export class DigitrafficStatisticsStack extends cdk.Stack {
 
-    constructor(scope: Construct, id: string, customProps: DigitrafficStatisticsProps, props?: cdk.StackProps) {
+    readonly statisticsProps: DigitrafficStatisticsProps;
+
+    constructor(scope: Construct, id: string, statisticsProps: DigitrafficStatisticsProps, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const vpc = ec2.Vpc.fromLookup(this, "EsKeyFiguresVPC", {
-            vpcName: customProps.vpcName
-        });
+        this.statisticsProps = statisticsProps;
 
-        const digitrafficMonthlySg = new ec2.SecurityGroup(this, "digitraffic-monthly-sg", {
-            vpc,
-            allowAllOutbound: true,
-            securityGroupName: "digitraffic-monthly-sg"
-        });
+        const statisticsIntegrations = new StatisticsIntegrations(this);
+        const statisticsApi = new StatisticsApi(this, statisticsIntegrations);
 
-        const rdsEsKeyFiguresSg = ec2.SecurityGroup.fromLookupById(this, "es-key-figures-sg", customProps.rdsSgId);
-        rdsEsKeyFiguresSg.addIngressRule(
-            ec2.Peer.securityGroupId(digitrafficMonthlySg.securityGroupId),
-            ec2.Port.tcp(3306)
-        );
+        this.setupStatisticsDomain(this, statisticsApi);
+    }
 
-        const digitrafficMonthlyFunction = new lambda.DockerImageFunction(this, 'digitraffic-monthly', {
-            functionName: "digitraffic-monthly",
-            code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../digitraffic-figures/'), {}),
-            vpc: vpc,
-            securityGroups: [digitrafficMonthlySg],
-            role: this.createDigitrafficMonthlyLambdaRole(customProps.dbSecretArn),
-            architecture: lambda.Architecture.ARM_64,
-            memorySize: 4096,
-            timeout: Duration.seconds(60),
-            environment: customProps.figuresLambdaEnv
-        });
-        const digitrafficMonthlyLambdaIntegration = new apigw.LambdaIntegration(digitrafficMonthlyFunction, {
-            proxy: true
-        });
-
-        const apiStatisticsBucket = s3.Bucket.fromBucketArn(this, "api-statistics-bucket", customProps.visualizationsBucketArn);
-        const apiStatisticsS3Integration = new apigw.AwsIntegration({
-            service: "s3",
-            integrationHttpMethod: "GET",
-            path: `${apiStatisticsBucket.bucketName}/index.html`,
-            options: {
-                credentialsRole: this.createS3ExecutionRole(apiStatisticsBucket),
-                integrationResponses: [{
-                    statusCode: "200",
-                    responseParameters: {
-                        'method.response.header.Content-Type': "'text/html'",
-                    },
-                }]
-            }
-        })
-
-        const restApi = new apigw.RestApi(this, "digitraffic-statistics-api", {
-            restApiName: "digitraffic-statistics-api",
-            policy: this.createApiIpRestrictionPolicy(customProps.allowedIpAddresses),
-            deployOptions: {
-                stageName: "prod"
-            }
-        });
-
-        const digitrafficApiStatistics = restApi.root.addResource("digitraffic-api-statistics");
-        digitrafficApiStatistics.addMethod("GET", apiStatisticsS3Integration, {
-            methodResponses: [{
-                statusCode: "200",
-                responseParameters: {
-                    'method.response.header.Content-Type': true,
-                }
-            }]
-        });
-
-        const digitrafficMonthly = restApi.root.addResource("digitraffic-monthly",
-            {
-                defaultIntegration: digitrafficMonthlyLambdaIntegration,
-            });
-        digitrafficMonthly.addProxy({
-            defaultIntegration: digitrafficMonthlyLambdaIntegration,
-            anyMethod: true
-        })
-        digitrafficMonthly.addMethod("GET");
-
-        const domain = "statistics.digitraffic.fi";
-        const domainName = new apigw.DomainName(this, 'domain', {
-            domainName: domain,
-            certificate: acm.Certificate.fromCertificateArn(this, "digitraffic-statistics-certificate", customProps.certificateArn),
+    private setupStatisticsDomain(stack: DigitrafficStatisticsStack, statisticsApi: StatisticsApi) {
+        const domainName = stack.statisticsProps.domainName;
+        const apigwDomainName = new apigw.DomainName(this, 'domain', {
+            domainName: domainName,
+            certificate: acm.Certificate.fromCertificateArn(this, "digitraffic-statistics-certificate", stack.statisticsProps.certificateArn),
             endpointType: apigw.EndpointType.REGIONAL
         });
-        domainName.addBasePathMapping(restApi,{stage: restApi.deploymentStage});
+        apigwDomainName.addBasePathMapping(statisticsApi.restApi,{stage: statisticsApi.restApi.deploymentStage});
 
         const zone = route53.HostedZone.fromLookup(this, "digitraffic-statistics-zone", {
-            domainName: domain,
+            domainName: domainName,
         });
         new route53.ARecord(this, "digitraffic-statistics-api-dns", {
             zone: zone,
-            recordName: domain,
+            recordName: domainName,
             target: route53.RecordTarget.fromAlias(
-                new targets.ApiGatewayDomain(domainName)
+                new targets.ApiGatewayDomain(apigwDomainName)
             ),
         });
-    }
-
-    private createApiIpRestrictionPolicy(allowedIpAddresses: string[]): iam.PolicyDocument {
-        return new iam.PolicyDocument({
-            statements: [
-                new iam.PolicyStatement({
-                    effect: iam.Effect.ALLOW,
-                    actions: ["execute-api:Invoke"],
-                    principals: [new iam.AnyPrincipal()],
-                    resources: ["*"]
-                }),
-                new iam.PolicyStatement({
-                    effect: iam.Effect.DENY,
-                    actions: ["execute-api:Invoke"],
-                    conditions: {
-                        "NotIpAddress": {
-                            "aws:SourceIp": allowedIpAddresses
-                        }
-                    },
-                    principals: [new iam.AnyPrincipal()],
-                    resources: ["*"],
-                })
-            ]
-        })
-    }
-
-    private createDigitrafficMonthlyLambdaRole(dbSecretArn: string): iam.Role {
-        const lambdaRole = new iam.Role(this, "digitraffic-monthly-lambda-role", {
-            assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
-            roleName: "digitraffic-monthly-lambda-role",
-        });
-        lambdaRole.addToPolicy(
-            new iam.PolicyStatement({
-                effect: iam.Effect.ALLOW,
-                actions: [
-                    "ec2:CreateNetworkInterface",
-                    "logs:CreateLogStream",
-                    "ec2:DescribeNetworkInterfaces",
-                    "logs:DescribeLogGroups",
-                    "logs:DescribeLogStreams",
-                    "ec2:DeleteNetworkInterface",
-                    "logs:CreateLogGroup",
-                    "logs:PutLogEvents"
-                ],
-                resources: ["*"]
-            })
-        )
-        lambdaRole.addToPolicy(
-            new iam.PolicyStatement({
-                effect: iam.Effect.ALLOW,
-                actions: ["secretsmanager:GetSecretValue"],
-                resources: [dbSecretArn]
-            })
-        )
-        lambdaRole.addToPolicy(
-            new iam.PolicyStatement({
-                effect: iam.Effect.ALLOW,
-                actions: [
-                    "xray:PutTraceSegments",
-                    "xray:PutTelemetryRecords"
-                ],
-                resources: ["*"],
-            })
-        )
-        lambdaRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("CloudWatchLambdaInsightsExecutionRolePolicy"));
-
-        return lambdaRole;
-    }
-
-    private createS3ExecutionRole(bucket: s3.IBucket): iam.Role {
-        const executeRole = new iam.Role(this, "digitraffic-api-statistics-apigw-s3-integration-role", {
-            assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
-            roleName: "digitraffic-api-statistics-apigw-s3-integration-role",
-        });
-        executeRole.addToPolicy(
-            new iam.PolicyStatement({
-                resources: [`${bucket.bucketArn}/*`],
-                actions: ["s3:GetObject"],
-            })
-        );
-        return executeRole;
     }
 }
