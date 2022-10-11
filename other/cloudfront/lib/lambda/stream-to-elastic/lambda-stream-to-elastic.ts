a501a8a61f42cb0a9d4308bdc1c7fceda417b3e8
Jani Haakana <jani.k.haakana@solita.fi>
2022-10-11 12:40:21 +0300

    Remove unneeded export

--- other/cloudfront/lib/lambda/stream-to-elastic/lambda-stream-to-elastic.ts
+++ other/cloudfront/lib/lambda/stream-to-elastic/lambda-stream-to-elastic.ts
@@ -1,18 +1,18 @@
-import { KinesisStreamEvent, KinesisStreamRecord } from 'aws-lambda';
-import {findHeaderValue} from "./logging-util";
+import { KinesisStreamEvent, KinesisStreamRecord } from "aws-lambda";
+import { findHeaderValue } from "./logging-util";
 
 import * as AWSx from "aws-sdk";
 const AWS = AWSx as any;
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const zlib = require('zlib');
+const zlib = require("zlib");
 
 // camels
 /* eslint-disable camelcase */
 
 const appDomain = process.env.APP_DOMAIN as string;
-const application = appDomain.split('-')[0];
-const env = appDomain.split('-')[1];
+const application = appDomain.split("-")[0];
+const env = appDomain.split("-")[1];
 
 const elasticDomain = process.env.ELASTIC_DOMAIN as string;
 const endpoint = new AWS.Endpoint(elasticDomain);
@@ -21,7 +21,7 @@ const creds = new AWS.EnvironmentCredentials("AWS");
 const COMPRESS_OPTIONS = {
     level: 8,
     memLevel: 9,
-    chunkSize: 1024*16*1024,
+    chunkSize: 1024 * 16 * 1024,
 };
 
 // fields contains all the selected log fields in the order specified in loggint-utils.ts CLOUDFRONT_STREAMING_LOG_FIELDS
@@ -45,17 +45,17 @@ async function convertFieldNamesAndFormats(fields: string[]): Promise<any> {
     const acceptEncoding = fields[15];
     const headers = fields[16];
 
-    const digitrafficUser = findHeaderValue('digitraffic-user', headers);
-    const host = findHeaderValue('host', headers);
-    const xForwardedProto = findHeaderValue('X-Forwarded-Proto', headers);
+    const digitrafficUser = findHeaderValue("digitraffic-user", headers);
+    const host = findHeaderValue("host", headers);
+    const xForwardedProto = findHeaderValue("X-Forwarded-Proto", headers);
 
     return {
-        '@timestamp': timestamp,
-        '@edge_location': edgeLocation,
-        '@transport_type': application,
-        '@app': `${application}-cloudfront`,
-        '@env': env,
-        '@fields': {
+        "@timestamp": timestamp,
+        "@edge_location": edgeLocation,
+        "@transport_type": application,
+        "@app": `${application}-cloudfront`,
+        "@env": env,
+        "@fields": {
             remote_addr: ip,
             body_bytes_sent: +responseBytes,
             http_referrer: referrer,
@@ -78,12 +78,12 @@ async function convertFieldNamesAndFormats(fields: string[]): Promise<any> {
 }
 
 async function transformRecord(record: KinesisStreamRecord): Promise<any> {
-    const buffer = Buffer.from(record.kinesis.data, 'base64');
-    const cloudfrontRealtimeLogData: string = buffer.toString('utf8');
+    const buffer = Buffer.from(record.kinesis.data, "base64");
+    const cloudfrontRealtimeLogData: string = buffer.toString("utf8");
 
     //    console.log('line: ' + cloudfrontRealtimeLogData);
 
-    const logLineData = cloudfrontRealtimeLogData.trim().split('\t');
+    const logLineData = cloudfrontRealtimeLogData.trim().split("\t");
 
     return convertFieldNamesAndFormats(logLineData);
 }
@@ -91,27 +91,33 @@ async function transformRecord(record: KinesisStreamRecord): Promise<any> {
 function createIndexName(): string {
     const now = new Date();
     const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, "0");
 
     return `dt-nginx-${year}.${month}`;
 }
 
 export const handler = async (event: KinesisStreamEvent) => {
     try {
-        const action = { index: { _index: createIndexName(), _type: '_doc' } } as any;
+        const action = {
+            index: { _index: createIndexName(), _type: "_doc" },
+        } as any;
 
         //        console.log('using action ' + JSON.stringify(action));
 
-        const recordTransformPromises = event.Records.map(async (record: KinesisStreamRecord) => transformRecord(record));
+        const recordTransformPromises = event.Records.map(
+            async (record: KinesisStreamRecord) => transformRecord(record)
+        );
 
         const data = await Promise.all(recordTransformPromises);
-        const returnValue = await sendMessageToEs(createBulkMessage(action, data));
+        const returnValue = await sendMessageToEs(
+            createBulkMessage(action, data)
+        );
 
         if (returnValue.length < 200) {
             console.log("return value " + returnValue);
         }
     } catch (e) {
-        console.log('exception: ' + e);
+        console.log("exception: " + e);
         throw e;
     }
 };
@@ -119,12 +125,12 @@ export const handler = async (event: KinesisStreamEvent) => {
 function createBulkMessage(action: any, lines: any[]): string {
     let message = "";
 
-    lines.forEach(line => {
-        message+= JSON.stringify(action) + '\n';
-        message+= JSON.stringify(line) + '\n';
+    lines.forEach((line) => {
+        message += JSON.stringify(action) + "\n";
+        message += JSON.stringify(line) + "\n";
     });
 
-    message += '\n';
+    message += "\n";
 
     return message;
 }
@@ -144,29 +150,33 @@ function sendMessageToEs(message: string): Promise<any> {
     const signer = new AWS.Signers.V4(request, "es");
     signer.addAuthorization(creds, new Date());
 
-    console.log("sending POST to es unCompressedSize=%d requestSize=%d", message.length, request.body.length);
+    console.log(
+        "sending POST to es unCompressedSize=%d requestSize=%d",
+        message.length,
+        request.body.length
+    );
 
     const client = new AWS.NodeHttpClient();
     return new Promise((resolve, reject) => {
-        client.handleRequest(request,
+        client.handleRequest(
+            request,
             null,
-            function(httpResp: any) {
+            function (httpResp: any) {
                 let respBody = "";
 
                 console.log("statuscode %d", httpResp.statusCode);
 
-                httpResp.on("data", function(chunk: any) {
+                httpResp.on("data", function (chunk: any) {
                     respBody += chunk;
                 });
-                httpResp.on("end", function(chunk: any) {
+                httpResp.on("end", function (chunk: any) {
                     resolve(respBody);
                 });
             },
-            function(err: any) {
+            function (err: any) {
                 console.log("Error: " + err);
                 reject(new Error(err));
-            });
+            }
+        );
     });
 }
-
-exports.handler = handler;
