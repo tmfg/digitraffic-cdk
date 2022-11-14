47c9ab52af8f10b14b475f4bdadb5e2ed2e4c19d
Jouni Sonninen <jouni.sonninen@solita.fi>
2022-11-14 10:20:26 +0100

    Fixes canary

--- road/maintenance-tracking-municipality/lib/canaries/mtm-db.ts
+++ road/maintenance-tracking-municipality/lib/canaries/mtm-db.ts
@@ -10,43 +10,43 @@ export const handler = () => {
     const checker = DatabaseChecker.createForProxy();
 
     checker.notEmpty(
-        `Domain ${domainName} is not found`,
-        `SELECT * FROM maintenance_tracking_domain WHERE name = '${domainName}'`
+        `Domain ${domainName} exists`,
+        `SELECT count(*) FROM maintenance_tracking_domain WHERE name = '${domainName}'`
     );
 
     checker.notEmpty(
-        `Domain ${domainName} doesn't have any contracts`,
-        `SELECT * FROM maintenance_tracking_domain_contract WHERE domain = '${domainName}'`
+        `Domain ${domainName} has any contracts`,
+        `SELECT count(*) FROM maintenance_tracking_domain_contract WHERE domain = '${domainName}'`
     );
 
     checker.empty(
-        `Domain ${domainName} has contracts without source information`,
-        `SELECT * FROM maintenance_tracking_domain_contract WHERE source IS NULL AND domain = '${domainName}'`
+        `Domain ${domainName} has only contracts with source information`,
+        `SELECT count(*) FROM maintenance_tracking_domain_contract WHERE source IS NULL AND domain = '${domainName}'`
     );
 
     checker.empty(
-        `Domain ${domainName} have contracts those data is not updated in last 48 hours`,
-        `SELECT * FROM maintenance_tracking_domain_contract WHERE data_last_updated < now() - interval '48 hours' AND domain = '${domainName}'`
+        `Domain ${domainName} contracts route data is updated in last 48 hours`,
+        `SELECT count(*) FROM maintenance_tracking_domain_contract WHERE data_last_updated < now() - interval '48 hours' AND domain = '${domainName}'`
     );
 
     checker.notEmpty(
-        `Domain ${domainName} doesn't have fresh data from last 48h`,
-        `SELECT * FROM maintenance_tracking WHERE maintenance_tracking.end_time > now() - interval '48 hours' AND domain = '${domainName}'`
+        `Domain ${domainName} has fresh data from last 48h`,
+        `SELECT count(*) FROM maintenance_tracking WHERE maintenance_tracking.end_time > now() - interval '48 hours' AND domain = '${domainName}'`
     );
 
     checker.notEmpty(
-        `Domain ${domainName} doesn't have task mappings`,
-        `select * from maintenance_tracking_domain_task_mapping WHERE domain = '${domainName}'`
+        `Domain ${domainName} has task mappings`,
+        `select count(*) from maintenance_tracking_domain_task_mapping WHERE domain = '${domainName}'`
     );
 
     checker.empty(
-        `Domain ${domainName} have unchecked task mappings`,
-        `select * from maintenance_tracking_domain_task_mapping WHERE domain = '${domainName}' AND info like ('%TODO%')`
+        `Domain ${domainName} doesn't have unchecked task mappings`,
+        `select count(*) from maintenance_tracking_domain_task_mapping WHERE domain = '${domainName}' AND info like ('%TODO%')`
     );
 
     checker.notEmpty(
-        "Data has not been checked in last hour",
-        `SELECT * from data_updated where data_type = '${DataType.MAINTENANCE_TRACKING_DATA_CHECKED}' and updated > now() - interval '1 hours' and subtype = '${domainName}'`
+        "Data has been checked in last hour",
+        `SELECT count(*) from data_updated where data_type = '${DataType.MAINTENANCE_TRACKING_DATA_CHECKED}' and updated > now() - interval '1 hours' and subtype = '${domainName}'`
     );
 
     return checker.expect();
