9a6b1fb390e81b95597e6f451144a13d18fdcdd2
Jouni Sonninen <jouni.sonninen@solita.fi>
2023-01-19 22:46:27 +0100

    DPO-2003 Statuspage API päivittyy

--- other/status/lib/api/statuspage.ts
+++ other/status/lib/api/statuspage.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { MediaType } from "@digitraffic/common/dist/aws/types/mediatypes";
 
 const STATUSPAGE_API = "https://api.statuspage.io/v1/pages";
+const PAGE_SIZE = 100;
 
 export enum StatuspageComponentStatus {
     operational = "operational",
@@ -28,49 +29,96 @@ export class StatuspageApi {
     }
 
     async getStatuspageComponents(): Promise<StatuspageComponent[]> {
-        console.log("Fetching Statuspage components");
-        const resp = await axios.get<StatuspageComponent[]>(
-            `${STATUSPAGE_API}/${this.statuspagePageId}/components`,
-            {
-                headers: {
-                    Authorization: `OAuth ${this.statuspageApiKey}`,
-                },
+        console.log(`method=getStatuspageComponents`);
+
+        let dataLength = 0;
+        let page = 1;
+        let statuspageComponents: StatuspageComponent[] = [];
+
+        do {
+            const url = `${STATUSPAGE_API}/${this.statuspagePageId}/components?per_page=${PAGE_SIZE}&page=${page}`;
+            const resp = await axios
+                .get<StatuspageComponent[]>(url, {
+                    headers: {
+                        Authorization: `OAuth ${this.statuspageApiKey}`,
+                    },
+                })
+                .catch((reason: AxiosError) => {
+                    throw new Error(
+                        `method=getStatuspageComponents Unable to get Statuspage components. Error ${
+                            reason.code ? reason.code : ""
+                        } ${reason.message}`
+                    );
+                });
+
+            if (resp.status !== 200) {
+                throw new Error(
+                    `method=getStatuspageComponents Unable to get Statuspage components status: HTTP ${resp.status} ${resp.statusText}`
+                );
             }
+
+            dataLength = resp.data.length;
+            console.log(
+                `method=getStatuspageComponents ${url} got ${dataLength} components`
+            );
+            if (dataLength > 0) {
+                statuspageComponents = statuspageComponents.concat(resp.data);
+            }
+            page++;
+        } while (dataLength > 0);
+
+        console.log(
+            `method=getStatuspageComponents got ${statuspageComponents.length} components`
         );
-        if (resp.status !== 200) {
-            throw new Error("Unable to get Statuspage components");
-        }
-        console.log("..done");
-        return resp.data;
+        return statuspageComponents;
     }
 
     async createStatuspageComponent(
         endpoint: string,
         statuspageComponentGroupId: string
     ) {
-        console.log("Creating Statuspage component for endpoint", endpoint);
-        const resp = await axios.post(
-            `${STATUSPAGE_API}/${this.statuspagePageId}/components`,
-            {
-                component: {
-                    description: endpoint,
-                    status: "operational",
-                    name: endpoint,
-                    only_show_if_degraded: "false",
-                    group_id: statuspageComponentGroupId,
-                    showcase: "true",
-                },
-            },
-            {
-                headers: {
-                    Authorization: `OAuth ${this.statuspageApiKey}`,
-                    "Content-type": MediaType.APPLICATION_JSON,
-                },
-            }
+        console.log(
+            "method=createStatuspageComponent Creating Statuspage component for endpoint",
+            endpoint
         );
+        const resp = await axios
+            .post(
+                `${STATUSPAGE_API}/${this.statuspagePageId}/components`,
+                {
+                    component: {
+                        description: endpoint,
+                        status: "operational",
+                        name: endpoint,
+                        only_show_if_degraded: "false",
+                        group_id: statuspageComponentGroupId,
+                        showcase: "true",
+                    },
+                },
+                {
+                    headers: {
+                        Authorization: `OAuth ${this.statuspageApiKey}`,
+                        "Content-type": MediaType.APPLICATION_JSON,
+                    },
+                }
+            )
+            .catch((reason: AxiosError) => {
+                throw new Error(
+                    `method=createStatuspageComponent Unable to create Statuspage component for endpoint ${endpoint}. ` +
+                        `Error ${reason.code ? reason.code : ""} ${
+                            reason.message
+                        }`
+                );
+            });
+
         if (resp.status !== 201) {
-            throw new Error("Unable to create Statuspage component");
+            throw new Error(
+                `method=createStatuspageComponent Unable to Statuspage component for endpoint ${endpoint}. ` +
+                    `HTTP ${resp.status} ${resp.statusText}`
+            );
         }
-        console.log("..done");
+
+        console.log(
+            `method=createStatuspageComponent for endpoint ${endpoint} done`
+        );
     }
 }
