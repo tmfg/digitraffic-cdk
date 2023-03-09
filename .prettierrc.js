205c21f61f732c3156b83baa4b8d2a1cac6359f4
Jani Haakana <jani.k.haakana@solita.fi>
2023-03-09 15:13:53 +0200

    Use 110 line length and use pretty-quick instead of lint-staged

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    plugins: ["prettier-plugin-packagejson"],
+
+    printWidth: 110,
+    endOfLine: "auto",
+    singleQuote: false,
+
+    // For ES5, trailing commas cannot be used in function parameters
+    trailingComma: "none",
+    overrides: [
+        {
+            files: ["*.json", "*.yml", "*.yaml"],
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
+};
