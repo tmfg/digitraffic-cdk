a9365e748823ab45a207a4cdaf37314071af50e5
Jouni Sonninen <jouni.sonninen@solita.fi>
2023-04-03 09:12:25 +0200

    DPO-2042 Kunnossapitodatan haku tuottaa kantakuormaa - update timeout for db clean lambda

--- road/maintenance-tracking/lib/internal-lambdas.ts
+++ road/maintenance-tracking/lib/internal-lambdas.ts
@@ -1,16 +1,11 @@
 import { Scheduler } from "@digitraffic/common/dist/aws/infra/scheduler";
 import {
     MonitoredDBFunction,
-    MonitoredFunction,
+    MonitoredFunction
 } from "@digitraffic/common/dist/aws/infra/stack/monitoredfunction";
 import { DigitrafficStack } from "@digitraffic/common/dist/aws/infra/stack/stack";
 import { createSubscription } from "@digitraffic/common/dist/aws/infra/stack/subscription";
-import {
-    ManagedPolicy,
-    PolicyStatement,
-    Role,
-    ServicePrincipal,
-} from "aws-cdk-lib/aws-iam";
+import { ManagedPolicy, PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Queue } from "aws-cdk-lib/aws-sqs";
@@ -19,25 +14,15 @@ import { MaintenanceTrackingEnvKeys } from "./keys";
 import { MaintenanceTrackingStackConfiguration } from "./maintenance-tracking-stack-configuration";
 import { QueueAndDLQ } from "./sqs";
 
-export function createCleanMaintenanceTrackingDataLambda(
-    stack: DigitrafficStack
-): MonitoredFunction {
+export function createCleanMaintenanceTrackingDataLambda(stack: DigitrafficStack): MonitoredFunction {
     const environment = stack.createLambdaEnvironment();
 
-    const lambdaFunction = MonitoredDBFunction.create(
-        stack,
-        "clean-maintenance-tracking-data",
-        environment,
-        {
-            memorySize: 128,
-        }
-    );
+    const lambdaFunction = MonitoredDBFunction.create(stack, "clean-maintenance-tracking-data", environment, {
+        memorySize: 128,
+        timeout: 600
+    });
 
-    Scheduler.everyHour(
-        stack,
-        `MaintenanceTracking-cleanMaintenanceTrackingDataEveryHour`,
-        lambdaFunction
-    );
+    Scheduler.everyHour(stack, `MaintenanceTracking-cleanMaintenanceTrackingDataEveryHour`, lambdaFunction);
 
     return lambdaFunction;
 }
@@ -56,12 +41,7 @@ export function createProcessQueueAndDlqLambda(
         stackConfiguration,
         stack
     );
-    createProcessDLQLambda(
-        dlqBucket,
-        queueAndDLQ.dlq,
-        stackConfiguration,
-        stack
-    );
+    createProcessDLQLambda(dlqBucket, queueAndDLQ.dlq, stackConfiguration, stack);
 }
 
 function createProcessQueueLambda(
@@ -71,34 +51,25 @@ function createProcessQueueLambda(
     stackConfiguration: MaintenanceTrackingStackConfiguration,
     stack: DigitrafficStack
 ) {
-    const role = createLambdaRoleWithReadS3Policy(
-        stack,
-        sqsExtendedMessageBucketArn
-    );
+    const role = createLambdaRoleWithReadS3Policy(stack, sqsExtendedMessageBucketArn);
 
     const secret = stack.secret;
 
     const lambdaEnv = stack.createLambdaEnvironment();
-    lambdaEnv[MaintenanceTrackingEnvKeys.SQS_BUCKET_NAME] =
-        stackConfiguration.sqsMessageBucketName;
+    lambdaEnv[MaintenanceTrackingEnvKeys.SQS_BUCKET_NAME] = stackConfiguration.sqsMessageBucketName;
     lambdaEnv[MaintenanceTrackingEnvKeys.SQS_QUEUE_URL] = queue.queueUrl;
 
-    const processQueueLambda = MonitoredFunction.createV2(
-        stack,
-        "process-queue",
-        lambdaEnv,
-        {
-            reservedConcurrentExecutions: 100,
-            timeout: 60,
-            role: role,
-            memorySize: 256,
-        }
-    );
+    const processQueueLambda = MonitoredFunction.createV2(stack, "process-queue", lambdaEnv, {
+        reservedConcurrentExecutions: 100,
+        timeout: 60,
+        role: role,
+        memorySize: 256
+    });
 
     // Handle only one message per time
     processQueueLambda.addEventSource(
         new SqsEventSource(queue, {
-            batchSize: 1,
+            batchSize: 1
         })
     );
     secret?.grantRead(processQueueLambda);
@@ -117,19 +88,13 @@ function createProcessDLQLambda(
     stack: DigitrafficStack
 ) {
     const lambdaEnv = stack.createLambdaEnvironment();
-    lambdaEnv[MaintenanceTrackingEnvKeys.SQS_DLQ_BUCKET_NAME] =
-        dlqBucket.bucketName;
+    lambdaEnv[MaintenanceTrackingEnvKeys.SQS_DLQ_BUCKET_NAME] = dlqBucket.bucketName;
 
-    const processDLQLambda = MonitoredFunction.createV2(
-        stack,
-        "process-dlq",
-        lambdaEnv,
-        {
-            reservedConcurrentExecutions: 1,
-            timeout: 10,
-            memorySize: 256,
-        }
-    );
+    const processDLQLambda = MonitoredFunction.createV2(stack, "process-dlq", lambdaEnv, {
+        reservedConcurrentExecutions: 1,
+        timeout: 10,
+        memorySize: 256
+    });
 
     processDLQLambda.addEventSource(new SqsEventSource(dlq));
 
@@ -147,13 +112,10 @@ function createProcessDLQLambda(
     processDLQLambda.addToRolePolicy(statement);
 }
 
-function createLambdaRoleWithReadS3Policy(
-    stack: Construct,
-    sqsExtendedMessageBucketArn: string
-): Role {
+function createLambdaRoleWithReadS3Policy(stack: Construct, sqsExtendedMessageBucketArn: string): Role {
     const lambdaRole = new Role(stack, `ReadSqsExtendedMessageBucketRole`, {
         assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
-        roleName: `ReadSqsExtendedMessageBucketRole`,
+        roleName: `ReadSqsExtendedMessageBucketRole`
     });
 
     const s3PolicyStatement = new PolicyStatement();
@@ -163,14 +125,10 @@ function createLambdaRoleWithReadS3Policy(
 
     lambdaRole.addToPolicy(s3PolicyStatement);
     lambdaRole.addManagedPolicy(
-        ManagedPolicy.fromAwsManagedPolicyName(
-            "service-role/AWSLambdaBasicExecutionRole"
-        )
+        ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")
     );
     lambdaRole.addManagedPolicy(
-        ManagedPolicy.fromAwsManagedPolicyName(
-            "service-role/AWSLambdaVPCAccessExecutionRole"
-        )
+        ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaVPCAccessExecutionRole")
     );
     return lambdaRole;
 }
