735822acbe673cfc9171c083e0dfc58189b9e8fe
Ilari Junnonen <ilari.junnonen@solita.fi>
2023-04-04 09:21:19 +0300

    Check for existing key figure also by name and filter

--- other/es-key-figures/lib/lambda/collect-es-key-figures.ts
+++ other/es-key-figures/lib/lambda/collect-es-key-figures.ts
@@ -7,25 +7,9 @@ import mysql from "mysql";
 const endpoint = new AWS.Endpoint(process.env.ES_ENDPOINT as string);
 
 const currentDate = new Date();
-const startDate = new Date(
-    currentDate.getFullYear(),
-    currentDate.getMonth() - 1,
-    1,
-    0,
-    0,
-    0,
-    0
-);
+const startDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1, 0, 0, 0, 0);
 
-const endDate = new Date(
-    currentDate.getFullYear(),
-    currentDate.getMonth() - 0,
-    1,
-    0,
-    0,
-    0,
-    0
-);
+const endDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 0, 1, 0, 0, 0, 0);
 
 const KEY_FIGURES_TABLE_NAME = "key_figures";
 const DUPLICATES_TABLE_NAME = "duplicates";
@@ -34,7 +18,7 @@ const connection = mysql.createConnection({
     host: process.env.MYSQL_ENDPOINT,
     user: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: process.env.MYSQL_DATABASE
 });
 
 const query = (sql: string, values?: string[]) => {
@@ -66,9 +50,7 @@ export interface KeyFigureLambdaEvent {
 }
 
 export const handler = async (event: KeyFigureLambdaEvent): Promise<boolean> => {
-    const apiPaths = (await getApiPaths()).filter(
-        (s) => s.transportType === event.TRANSPORT_TYPE
-    );
+    const apiPaths = (await getApiPaths()).filter((s) => s.transportType === event.TRANSPORT_TYPE);
 
     const pathsToProcess = [...apiPaths[0].paths];
     const middleIndex = Math.ceil(pathsToProcess.length / 2);
@@ -117,40 +99,24 @@ async function getKibanaResult(
             query: query,
             filter: filter,
             name: keyFigure.name,
-            value: undefined,
+            value: undefined
         };
 
         if (keyFigure.type === "count") {
-            const keyFigureResponse = await fetchDataFromEs(
-                endpoint,
-                query,
-                "_count"
-            );
+            const keyFigureResponse = await fetchDataFromEs(endpoint, query, "_count");
             keyFigureResult.value = keyFigureResponse.count;
         } else if (keyFigure.type === "agg") {
-            const keyFigureResponse = await fetchDataFromEs(
-                endpoint,
-                query,
-                "_search?size=0"
-            );
+            const keyFigureResponse = await fetchDataFromEs(endpoint, query, "_search?size=0");
             keyFigureResult.value = keyFigureResponse.aggregations.agg.value;
         } else if (keyFigure.type === "field_agg") {
-            const keyFigureResponse = await fetchDataFromEs(
-                endpoint,
-                query,
-                "_search?size=0"
-            );
+            const keyFigureResponse = await fetchDataFromEs(endpoint, query, "_search?size=0");
             const value: { [key: string]: unknown } = {};
             for (const bucket of keyFigureResponse.aggregations.agg.buckets) {
                 value[removeIllegalChars(bucket.key)] = bucket.doc_count;
             }
             keyFigureResult.value = value;
         } else if (keyFigure.type === "sub_agg") {
-            const keyFigureResponse = await fetchDataFromEs(
-                endpoint,
-                query,
-                "_search?size=0"
-            );
+            const keyFigureResponse = await fetchDataFromEs(endpoint, query, "_search?size=0");
             const value: { [key: string]: unknown } = {};
             for (const bucket of keyFigureResponse.aggregations.agg.buckets) {
                 value[removeIllegalChars(bucket.key)] = bucket.agg.value;
@@ -204,31 +170,28 @@ async function getKibanaResults(
     return kibanaResults.flat();
 }
 
-async function getRowAmountWithDate(isoDate: string): Promise<number> {
+async function getRowAmountWithDateNameFilter(
+    isoDate: string,
+    name: string,
+    filter: string
+): Promise<number> {
     try {
         const resultKey = "count";
         const existingRowsFromDate = await query(
-            "SELECT COUNT(*) AS ? FROM ?? WHERE `from` = ?;",
-            [resultKey, KEY_FIGURES_TABLE_NAME, isoDate]
+            "SELECT COUNT(*) AS ? FROM ?? WHERE `from` = ? AND `name` = ? AND `filter` = ?;",
+            [resultKey, KEY_FIGURES_TABLE_NAME, isoDate, name, filter]
         );
-        return Promise.resolve(
-            (existingRowsFromDate as Record<string, unknown>[])[0][
-                resultKey
-            ] as number
-        );
+        return Promise.resolve((existingRowsFromDate as Record<string, unknown>[])[0][resultKey] as number);
     } catch (error: unknown) {
         console.error("Error querying database: ", error);
         throw error;
     }
 }
 
-async function insertFigures(
-    kibanaResults: KeyFigureResult[],
-    tableName: string
-) {
+async function insertFigures(kibanaResults: KeyFigureResult[], tableName: string) {
     for (const result of kibanaResults) {
-            // prettier-ignore
-            await query(`INSERT INTO \`${tableName}\` (\`from\`, \`to\`, \`query\`, \`value\`, \`name\`, \`filter\`)
+        // prettier-ignore
+        await query(`INSERT INTO \`${tableName}\` (\`from\`, \`to\`, \`query\`, \`value\`, \`name\`, \`filter\`)
                          VALUES ('${startDate.toISOString().substring(0, 10)}', '${endDate.toISOString().substring(0, 10)}', '${result.query}',
                                  '${JSON.stringify(result.value)}', '${result.name}', '${result.filter}');`);
     }
@@ -237,8 +200,7 @@ async function insertFigures(
 async function persistToDatabase(kibanaResults: KeyFigureResult[]) {
     const CREATE_KEY_FIGURES_TABLE =
         "CREATE TABLE ?? ( `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `from` DATE NOT NULL, `to` DATE NOT NULL, `name` VARCHAR(100) NOT NULL,`filter` VARCHAR(1000) NOT NULL, `query` VARCHAR(1000) NOT NULL, `value` JSON NOT NULL, PRIMARY KEY (`id`))";
-    const CREATE_KEY_FIGURES_INDEX =
-        "CREATE INDEX filter_name_date ON ?? (`filter`, `name`, `from`, `to`);";
+    const CREATE_KEY_FIGURES_INDEX = "CREATE INDEX filter_name_date ON ?? (`filter`, `name`, `from`, `to`);";
 
     try {
         const tables = await query("show tables");
@@ -249,12 +211,16 @@ async function persistToDatabase(kibanaResults: KeyFigureResult[]) {
         }
 
         const startIsoDate = startDate.toISOString().substring(0, 10);
-        const existingRows = await getRowAmountWithDate(startIsoDate);
+        const existingRows = await getRowAmountWithDateNameFilter(
+            startIsoDate,
+            kibanaResults[0].name,
+            kibanaResults[0].filter
+        );
 
-        // save duplicate rows to a separate table if current results already exist in database
+        // save duplicate rows to a separate table if current set of results already exists in database
         if (existingRows > 0) {
             console.info(
-                `Found ${existingRows} rows where 'from' is '${startIsoDate}', saving to table ${DUPLICATES_TABLE_NAME}`
+                `Found existing result '${kibanaResults[0].name}' where 'from' is '${startIsoDate}' and filter is '${kibanaResults[0].filter}', saving to table ${DUPLICATES_TABLE_NAME}`
             );
             await query("DROP TABLE IF EXISTS ??", [DUPLICATES_TABLE_NAME]);
             await query(CREATE_KEY_FIGURES_TABLE, [DUPLICATES_TABLE_NAME]);
@@ -269,18 +235,10 @@ async function persistToDatabase(kibanaResults: KeyFigureResult[]) {
     }
 }
 
-async function getApiPaths(): Promise<
-    { transportType: string; paths: Set<string> }[]
-> {
-    const railSwaggerPaths = await getPaths(
-        "https://rata.digitraffic.fi/swagger/openapi.json"
-    );
-    const roadSwaggerPaths = await getPaths(
-        "https://tie.digitraffic.fi/swagger/openapi.json"
-    );
-    const marineSwaggerPaths = await getPaths(
-        "https://meri.digitraffic.fi/swagger/openapi.json"
-    );
+async function getApiPaths(): Promise<{ transportType: string; paths: Set<string> }[]> {
+    const railSwaggerPaths = await getPaths("https://rata.digitraffic.fi/swagger/openapi.json");
+    const roadSwaggerPaths = await getPaths("https://tie.digitraffic.fi/swagger/openapi.json");
+    const marineSwaggerPaths = await getPaths("https://meri.digitraffic.fi/swagger/openapi.json");
 
     railSwaggerPaths.add("/api/v2/graphql/");
     railSwaggerPaths.add("/api/v1/trains/history");
@@ -294,20 +252,20 @@ async function getApiPaths(): Promise<
     return [
         {
             transportType: "*",
-            paths: new Set(),
+            paths: new Set()
         },
         {
             transportType: "rail",
-            paths: railSwaggerPaths,
+            paths: railSwaggerPaths
         },
         {
             transportType: "road",
-            paths: roadSwaggerPaths,
+            paths: roadSwaggerPaths
         },
         {
             transportType: "marine",
-            paths: marineSwaggerPaths,
-        },
+            paths: marineSwaggerPaths
+        }
     ];
 }
 
@@ -319,7 +277,7 @@ function getKeyFigures(): KeyFigure[] {
 
 export async function getPaths(endpointUrl: string): Promise<Set<string>> {
     const resp = await axios.get(endpointUrl, {
-        headers: { "accept-encoding": "gzip" },
+        headers: { "accept-encoding": "gzip" }
     });
 
     if (resp.status !== 200) {
@@ -333,9 +291,7 @@ export async function getPaths(endpointUrl: string): Promise<Set<string>> {
     const output = new Set<string>();
     for (const pathsKey in paths) {
         const splitResult = pathsKey.split("{");
-        output.add(
-            splitResult[0].endsWith("/") ? splitResult[0] : splitResult[0] + "/"
-        );
+        output.add(splitResult[0].endsWith("/") ? splitResult[0] : splitResult[0] + "/");
     }
 
     return output;
