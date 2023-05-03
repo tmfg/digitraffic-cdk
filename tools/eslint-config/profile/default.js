97e8bc111d416ab83f67420637f0d3c518d2f71d
Jani Haakana <jani.k.haakana@solita.fi>
2023-05-03 09:44:23 +0300

    Do not check no-new rule against cdk code.

--- tools/eslint-config/profile/default.js
+++ tools/eslint-config/profile/default.js
@@ -102,8 +102,9 @@ module.exports = {
             }
         },
         {
-            // CDK has some quirks and thus shoudn't
+            // CDK has some quirks and thus shouldn't apply all the rules against them.
             files: [
+                "bin/**/*.ts",
                 "*.cdk.ts",
 
                 // old way
