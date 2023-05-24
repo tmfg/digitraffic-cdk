e6568ccecb9202ed69af875de14d414346a26236
jouniso <jouniso@users.noreply.github.com>
2023-05-24 09:49:43 +0200

    Fix lam history update apikey && eslint (#103)
    * Fix lam history update apikey && eslint

--- road/lam-history/lib/lambda/update-lam-stations/update-lam-stations.ts
+++ road/lam-history/lib/lambda/update-lam-stations/update-lam-stations.ts
@@ -2,35 +2,22 @@ import { SecretHolder } from "@digitraffic/common/dist/aws/runtime/secrets/secre
 import { getEnvVariable } from "@digitraffic/common/dist/utils/utils";
 import { LamHistoryEnvKeys } from "../../keys";
 
-import { LamHistorySecret } from "../../model/lam-history-secret";
+import { TmsHistorySecret } from "../../model/tms-history-secret";
 import { handleMetadataUpdate } from "../../service/update";
 
 const bucketName = getEnvVariable(LamHistoryEnvKeys.BUCKET_NAME);
-const secretHolder = SecretHolder.create<LamHistorySecret>("lam-history");
+const secretHolder = SecretHolder.create<TmsHistorySecret>("tms-history");
 
-export const handler = async () => {
+export const handler = async (): Promise<void> => {
     const start = Date.now();
 
     const secret = await secretHolder.get();
 
     // Update piste-data
-    await handleMetadataUpdate(
-        secret.pisteUrl,
-        secret.apikey,
-        bucketName,
-        "pisteet.json"
-    );
+    await handleMetadataUpdate(secret.pisteUrl, secret.snowflakeApikey, bucketName, "pisteet.json");
 
     // Update pistejoukko-data
-    await handleMetadataUpdate(
-        secret.pistejoukkoUrl,
-        secret.apikey,
-        bucketName,
-        "pistejoukot.json"
-    );
+    await handleMetadataUpdate(secret.pistejoukkoUrl, secret.snowflakeApikey, bucketName, "pistejoukot.json");
 
-    console.info(
-        "method=lamHistory.updateMetadata tookMs=%d",
-        Date.now() - start
-    );
+    console.info("method=lamHistory.updateMetadata tookMs=%d", Date.now() - start);
 };
