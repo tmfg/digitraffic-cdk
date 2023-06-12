b35bc884a83b5e487230cfa0279b8ca67c94329f
Ilari Junnonen <ilari.junnonen@solita.fi>
2023-06-12 08:55:34 +0300

    DPO-1822 Split up message schema

--- rail/rami/lib/model/rami-message.ts
+++ rail/rami/lib/model/rami-message.ts
@@ -1,30 +1,22 @@
-import { ramiMessageSchema } from "./rami-message-schema";
-import { z } from "zod";
+import type { ValueOf } from "@digitraffic/common/dist/types/util-types.js";
+import type { z } from "zod";
+import type { ramiMessageSchema } from "./schema/rami-message-schema.js";
+import type {
+    recipientAudioMessagesToDeliver,
+    recipientVideoMessagesToDeliver
+} from "./schema/scheduled-message-schema.js";
 
 export type RamiMessage = z.infer<typeof ramiMessageSchema>;
 export type RamiMessagePayload = Pick<RamiMessage, "payload">["payload"];
 
-export enum RamiMessageType {
-    MONITORED_JOURNEY_SCHEDULED_MESSAGE = "MONITORED_JOURNEY_SCHEDULED_MESSAGE",
-    SCHEDULED_MESSAGE = "SCHEDULED_MESSAGE"
-}
+export const RamiMessageTypes = {
+    MONITORED_JOURNEY_SCHEDULED_MESSAGE: "MONITORED_JOURNEY_SCHEDULED_MESSAGE",
+    SCHEDULED_MESSAGE: "SCHEDULED_MESSAGE"
+} as const;
+export type RamiMessageType = ValueOf<typeof RamiMessageTypes>;
 
-// only onGroundRecipient recipient type is used in practice with scheduledMessages
-type RamiScheduledMessageRecipient = Pick<
-    NonNullable<RamiMessage["payload"]["scheduledMessage"]>,
-    "onGroundRecipient"
->["onGroundRecipient"];
+export const RamiMessageOperations = { INSERT: "INSERT", UPDATE: "UPDATE", DELETE: "DELETE" } as const;
+export type RamiMessageOperation = ValueOf<typeof RamiMessageOperations>;
 
-export type RamiScheduledMessageVideo = NonNullable<
-    Pick<
-        NonNullable<RamiScheduledMessageRecipient>,
-        "recipientVideoMessagesToDeliver"
-    >["recipientVideoMessagesToDeliver"]
->;
-
-export type RamiScheduledMessageAudio = NonNullable<
-    Pick<
-        NonNullable<RamiScheduledMessageRecipient>,
-        "recipientAudioMessagesToDeliver"
-    >["recipientAudioMessagesToDeliver"]
->;
+export type RamiScheduledMessageVideo = NonNullable<z.infer<typeof recipientVideoMessagesToDeliver>>;
+export type RamiScheduledMessageAudio = NonNullable<z.infer<typeof recipientAudioMessagesToDeliver>>;
