705a1c295e0a24847463b8b14c6a485aed1bdf48
Jani Haakana <jani.k.haakana@solita.fi>
2023-09-06 09:02:44 +0300

    Init repo-scripts project

---
+++ lib/repo-scripts/src/check-dependencies.ts
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+import { globby } from "globby";
+import fs from "fs-extra";
+import chalk from "chalk";
+
+function getPackageJsons() {
+    return globby([
+        "aviation/*/package.json",
+        "marine/*/package.json",
+        "lib/*/package.json",
+        "other/*/package.json",
+        "rail/*/package.json",
+        "road/*/package.json",
+        "tools/*/package.json"
+    ]);
+}
+
+function checkDependencies(packageJson) {
+    if ("dependencies" in packageJson) {
+        const dependencies = Object.entries(packageJson.dependencies);
+        return dependencies.filter(([key]) => key.startsWith("@types/"));
+    }
+    return [];
+}
+
+async function run() {
+    const packages = await getPackageJsons();
+    const dependencies = await Promise.all(
+        packages.map(async (file) => ({
+            project: file.replace("/package.json", ""),
+            items: checkDependencies(await fs.readJson(file))
+        }))
+    );
+    const results = dependencies.filter(({ items }) => items.length !== 0);
+    results.forEach(({ project, items }) => {
+        console.log(
+            `Project: ${chalk.bold(project)} has declared following as dependency instead of devDependency:`
+        );
+        items.forEach(([key]) => console.log(`- ${key}`));
+    });
+
+    if (results.length > 0) {
+        console.log(chalk.red("Check above errors"));
+        process.exitCode = 1;
+    }
+}
+
+await run();
