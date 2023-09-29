f18d39d28c633cd9322dcf05e2c0edb0c2494892
teijosol <teijo.ronka@solita.fi>
2023-09-29 14:02:30 +0300

    DPO-1620 update common -> update lambda node versions

--- marine/sse/lib/integration-api.ts
+++ marine/sse/lib/integration-api.ts
@@ -1,15 +1,7 @@
-import {
-    IModel,
-    IntegrationResponse,
-    Resource,
-    RestApi,
-} from "aws-cdk-lib/aws-apigateway";
+import { IModel, IntegrationResponse, Resource, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { databaseFunctionProps } from "@digitraffic/common/dist/aws/infra/stack/lambda-configs";
 import { createRestApi } from "@digitraffic/common/dist/aws/infra/stack/rest_apis";
-import {
-    addDefaultValidator,
-    addServiceModel,
-} from "@digitraffic/common/dist/utils/api-model";
+import { addDefaultValidator, addServiceModel } from "@digitraffic/common/dist/utils/api-model";
 import { ISecret } from "aws-cdk-lib/aws-secretsmanager";
 import { Construct } from "constructs";
 
@@ -18,46 +10,25 @@ import * as ApiResponseSchema from "./model/api-response-schema";
 import { createDefaultUsagePlan } from "@digitraffic/common/dist/aws/infra/usage-plans";
 import { createSubscription } from "@digitraffic/common/dist/aws/infra/stack/subscription";
 import {
-    corsMethod,
     defaultIntegration,
     getResponse,
     methodResponse,
-    RESPONSE_200_OK,
+    RESPONSE_200_OK
 } from "@digitraffic/common/dist/aws/infra/api/responses";
 import { MediaType } from "@digitraffic/common/dist/aws/types/mediatypes";
-import { MessageModel } from "@digitraffic/common/dist/aws/infra/api/response";
-import {
-    BAD_REQUEST_MESSAGE,
-    ERROR_MESSAGE,
-} from "@digitraffic/common/dist/aws/types/errors";
+import { DigitrafficMethodResponse, MessageModel } from "@digitraffic/common/dist/aws/infra/api/response";
+import { BAD_REQUEST_MESSAGE, ERROR_MESSAGE } from "@digitraffic/common/dist/aws/types/errors";
 import { DigitrafficStack } from "@digitraffic/common/dist/aws/infra/stack/stack";
 import { MonitoredFunction } from "@digitraffic/common/dist/aws/infra/stack/monitoredfunction";
 
-export function createIntegrationApiAndHandlerLambda(
-    secret: ISecret,
-    stack: DigitrafficStack
-) {
-    const integrationApi: RestApi = createRestApi(
-        stack,
-        "SSE-Integration",
-        "SSE Data Integration API"
-    );
+export function createIntegrationApiAndHandlerLambda(secret: ISecret, stack: DigitrafficStack) {
+    const integrationApi: RestApi = createRestApi(stack, "SSE-Integration", "SSE Data Integration API");
 
     const sseModel = addServiceModel("Sse", integrationApi, SseSchema.Sse);
-    const okResponseModel = addServiceModel(
-        "OkResponseModel",
-        integrationApi,
-        ApiResponseSchema.OkResponse
-    );
-    const errorResponseModel = integrationApi.addModel(
-        "ErrorResponseModel",
-        MessageModel
-    );
+    const okResponseModel = addServiceModel("OkResponseModel", integrationApi, ApiResponseSchema.OkResponse);
+    const errorResponseModel = integrationApi.addModel("ErrorResponseModel", MessageModel);
 
-    const apiResource = createUpdateSseApiGatewayResource(
-        stack,
-        integrationApi
-    );
+    const apiResource = createUpdateSseApiGatewayResource(stack, integrationApi);
     const updateSseDataLambda = createUpdateRequestHandlerLambda(
         apiResource,
         sseModel,
@@ -67,20 +38,11 @@ export function createIntegrationApiAndHandlerLambda(
     );
     secret.grantRead(updateSseDataLambda);
 
-    createDefaultUsagePlan(
-        integrationApi,
-        "SSE - Sea State Estimate Integration"
-    );
+    createDefaultUsagePlan(integrationApi, "SSE - Sea State Estimate Integration");
 }
 
-function createUpdateSseApiGatewayResource(
-    stack: Construct,
-    integrationApi: RestApi
-): Resource {
-    const apiResource = integrationApi.root
-        .addResource("sse")
-        .addResource("v1")
-        .addResource("update");
+function createUpdateSseApiGatewayResource(stack: Construct, integrationApi: RestApi): Resource {
+    const apiResource = integrationApi.root.addResource("sse").addResource("v1").addResource("update");
 
     addDefaultValidator(integrationApi);
     return apiResource;
@@ -99,55 +61,31 @@ function createUpdateRequestHandlerLambda(
     const updateSseDataLambda = MonitoredFunction.create(
         stack,
         lambdaFunctionName,
-        databaseFunctionProps(
-            stack,
-            environment,
-            lambdaFunctionName,
-            "lambda-update-sse-data",
-            {
-                singleLambda: true,
-                reservedConcurrentExecutions: 10,
-                memorySize: 256,
-            }
-        )
+        databaseFunctionProps(stack, environment, lambdaFunctionName, "lambda-update-sse-data", {
+            singleLambda: true,
+            reservedConcurrentExecutions: 10,
+            memorySize: 256
+        })
     );
 
     const lambdaIntegration = defaultIntegration(updateSseDataLambda, {
         responses: [
             getResponse(RESPONSE_200_OK),
             getResponse(RESPONSE_400_BAD_REQUEST),
-            getResponse(RESPONSE_500_SERVER_ERROR),
-        ],
+            getResponse(RESPONSE_500_SERVER_ERROR)
+        ]
     });
 
     requests.addMethod("POST", lambdaIntegration, {
         apiKeyRequired: true,
         requestModels: {
-            "application/json": sseRequestModel,
+            "application/json": sseRequestModel
         },
         methodResponses: [
-            corsMethod(
-                methodResponse(
-                    "200",
-                    MediaType.APPLICATION_JSON,
-                    okResponseModel
-                )
-            ),
-            corsMethod(
-                methodResponse(
-                    "400",
-                    MediaType.APPLICATION_JSON,
-                    errorResponseModel
-                )
-            ),
-            corsMethod(
-                methodResponse(
-                    "500",
-                    MediaType.APPLICATION_JSON,
-                    errorResponseModel
-                )
-            ),
-        ],
+            DigitrafficMethodResponse.response200(okResponseModel, MediaType.APPLICATION_JSON),
+            DigitrafficMethodResponse.response400(errorResponseModel, MediaType.APPLICATION_JSON),
+            DigitrafficMethodResponse.response500(errorResponseModel, MediaType.APPLICATION_JSON)
+        ]
     });
 
     // Create log subscription
@@ -165,8 +103,8 @@ const RESPONSE_400_BAD_REQUEST: IntegrationResponse = {
     selectionPattern: `.*${BAD_REQUEST_MESSAGE}.*`,
     responseTemplates: {
         "application/json":
-            '{ "message" : "Bad request. $util.parseJson($input.path(\'$.errorMessage\')).errorMessage" }',
-    },
+            '{ "message" : "Bad request. $util.parseJson($input.path(\'$.errorMessage\')).errorMessage" }'
+    }
 };
 
 const RESPONSE_500_SERVER_ERROR: IntegrationResponse = {
@@ -174,6 +112,6 @@ const RESPONSE_500_SERVER_ERROR: IntegrationResponse = {
     selectionPattern: `.*${ERROR_MESSAGE}.*`,
     responseTemplates: {
         "application/json":
-            '{ "message" : "Internal server error. $util.parseJson($input.path(\'$.errorMessage\')).errorMessage" }',
-    },
+            '{ "message" : "Internal server error. $util.parseJson($input.path(\'$.errorMessage\')).errorMessage" }'
+    }
 };
