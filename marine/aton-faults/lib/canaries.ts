90b9897f1d918159d517c5f9edb4c70763242305
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-01-11 14:08:22 +0200

    Remove unnecessary canaries from aton-faults

--- marine/aton-faults/lib/canaries.ts
+++ marine/aton-faults/lib/canaries.ts
@@ -1,6 +1,5 @@
 import { DigitrafficStack } from "@digitraffic/common/dist/aws/infra/stack/stack";
 import { UrlCanary } from "@digitraffic/common/dist/aws/infra/canaries/url-canary";
-import { DatabaseCanary } from "@digitraffic/common/dist/aws/infra/canaries/database-canary";
 import { DigitrafficCanaryRole } from "@digitraffic/common/dist/aws/infra/canaries/canary-role";
 import { DigitrafficRestApi } from "@digitraffic/common/dist/aws/infra/stack/rest_apis";
 
@@ -8,21 +7,15 @@ export class Canaries {
     constructor(stack: DigitrafficStack, publicApi: DigitrafficRestApi) {
         if (stack.configuration.stackFeatures?.enableCanaries ?? true) {
             const urlRole = new DigitrafficCanaryRole(stack, "aton-url");
-            const dbRole = new DigitrafficCanaryRole(
-                stack,
-                "aton-db"
-            ).withDatabaseAccess();
 
             UrlCanary.create(stack, urlRole, publicApi, {
                 name: "aton-public",
                 handler: "public-api.handler",
                 alarm: {
                     alarmName: "ATON-PublicAPI-Alarm",
-                    topicArn: stack.configuration.warningTopicArn,
-                },
+                    topicArn: stack.configuration.warningTopicArn
+                }
             });
-
-            DatabaseCanary.createV2(stack, dbRole, "aton");
         }
     }
 }
