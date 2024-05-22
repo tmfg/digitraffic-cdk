73e71a64f61c751ab37ea5d9bfc4a69c27989786
teijosol <teijo.ronka@solita.fi>
2024-05-22 11:31:47 +0300

    DPO-2535 dependency tests

---
+++ marine/gofrep/src/__test__/dependencies.test.ts
@@ -0,0 +1,8 @@
+import { DependencyTester } from "@digitraffic-cdk/testing";
+
+test("circular dependencies", async () => {
+    const tester = await DependencyTester.create("lib");
+
+    tester.assertNoCircularDependencies();
+    tester.assertNoOrphans();
+});
