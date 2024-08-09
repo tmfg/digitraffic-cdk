9c2a964c4c5071a28ab9820f4773c86c7d860de4
Joonas-M-S <joonas.mikkonen@solita.fi>
2024-08-09 11:01:02 +0300

    Use newer common s3 upload API

--- marine/voyage-plan-gateway/src/api/rtzstorage.ts
+++ marine/voyage-plan-gateway/src/api/rtzstorage.ts
@@ -1,6 +1,8 @@
 import * as S3Utils from "@digitraffic/common/dist/aws/runtime/s3";
 import { logger } from "@digitraffic/common/dist/aws/runtime/dt-logger-default";
+import { S3Client } from "@aws-sdk/client-s3";
 
+const s3 = new S3Client();
 export class RtzStorageApi {
     private readonly bucketName: string;
 
@@ -15,7 +17,7 @@ export class RtzStorageApi {
         });
 
         const objectName = `voyageplan-${new Date().toISOString()}.xml`;
-        await S3Utils.uploadToS3(this.bucketName, voyagePlan, objectName);
+        await S3Utils.uploadToS3(s3, this.bucketName, voyagePlan, objectName);
 
         logger.info({
             method: "RtzStorageApi.storeVoyagePlan",
--- marine/voyage-plan-gateway/src/lambda/process-dlq/lambda-process-dlq.ts
+++ marine/voyage-plan-gateway/src/lambda/process-dlq/lambda-process-dlq.ts
@@ -4,6 +4,10 @@ import { getEnvVariable } from "@digitraffic/common/dist/utils/utils";
 import { logger } from "@digitraffic/common/dist/aws/runtime/dt-logger-default";
 import type { SQSEvent, SQSRecord } from "aws-lambda";
 
+import { S3Client } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client();
+
 const bucketName = getEnvVariable(VoyagePlanEnvKeys.BUCKET_NAME);
 
 export const handler = async (event: SQSEvent): Promise<void[]> => {
@@ -15,7 +19,7 @@ export const handler = async (event: SQSEvent): Promise<void[]> => {
     });
 
     const uploads = event.Records.map((e: SQSRecord, idx: number) =>
-        uploadToS3(bucketName, e.body, `route-${millis}-${idx}.json`)
+        uploadToS3(s3, bucketName, e.body, `route-${millis}-${idx}.json`)
     );
 
     return Promise.all(uploads);
