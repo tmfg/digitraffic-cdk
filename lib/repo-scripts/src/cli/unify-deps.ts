cd484ea4d7c8fdd7e745fc5a4163ced93e6ed458
Jani Haakana <jani.k.haakana@solita.fi>
2024-08-21 14:53:54 +0300

    DPO-2251 create unify-deps

--- lib/repo-scripts/src/cli/unify-deps.ts
+++ lib/repo-scripts/src/cli/unify-deps.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env zx --quiet
 
-import { $, question } from "zx";
+import { $, chalk, question } from "zx";
 import _ from "lodash";
+import fs from "fs-extra";
+import async from "async";
 
 interface RushVersion {
     version: string;
@@ -30,8 +32,24 @@ interface RushJson {
     projects: RushProject[];
 }
 
-async function askVersion(check: RushCheck): Promise<string> {
-    console.log(`Dependency: ${check.dependencyName}`);
+interface DependencyVersion {
+    dependencyName: string;
+    version: string;
+}
+
+interface Dependencies {
+    [k: string]: DependencyVersion | undefined;
+}
+
+interface PackageJson {
+    name: string;
+    dependencies?: Record<string, string>;
+    devDependencies?: Record<string, string>;
+    peerDependencies?: Record<string, string>;
+}
+
+async function askVersion(check: RushCheck): Promise<DependencyVersion> {
+    console.log(`Dependency: ${chalk.blue(check.dependencyName)}`);
 
     const versions = _.chain(check.versions)
         .sortBy(({ version }) => version)
@@ -39,7 +57,7 @@ async function askVersion(check: RushCheck): Promise<string> {
         .value();
 
     versions.forEach((version) => {
-        console.log(`  Version: ${version.version}`);
+        console.log(`  Version: ${chalk.green(version.version)}`);
         version.projects.forEach((project) => {
             console.log(`    - ${project}`);
         });
@@ -51,26 +69,76 @@ async function askVersion(check: RushCheck): Promise<string> {
         throw new Error("Unexpected error: Couldn't get the version");
     }
 
-    const version = await question("Which version to use? ([left]/right/somethingelse)");
-
-    if (version === "" || version === "left") {
-        return left.version;
-    } else if (version === "right") {
-        return right.version;
-    } else {
-        return version;
+    if (check.dependencyName.includes("@types/")) {
+        // strip ^ and ~ from @types/ dependencies
+        left.version = _.trimStart(left.version, "^~");
     }
+
+    let version = await question(
+        `Which version to use (${chalk.green(left.version)}/${chalk.yellow(right.version)})? (${chalk.green(
+            "[left]"
+        )}/${chalk.yellow("(r)ight")}/(s)kip/somethingelse): `
+    );
+
+    const answer = version.toLowerCase();
+
+    if (answer === "" || answer === "left" || answer === "l") {
+        version = left.version;
+    } else if (answer === "skip" || answer === "s") {
+        version = "skip";
+    } else if (answer === "right" || answer === "r") {
+        version = right.version;
+    }
+
+    console.log(`Picked: ${chalk.cyan(version)}\n`);
+
+    return { dependencyName: check.dependencyName, version };
 }
 
-function pickVersion() {}
+function groupByDependency(dependencyVersions: DependencyVersion[]): Dependencies {
+    return _.chain(dependencyVersions)
+        .groupBy("dependencyName")
+        .mapValues((items) => _.head(items))
+        .value();
+}
+
+function updateVersions(dependencies: Dependencies, packageJson: PackageJson): PackageJson {
+    return _.mapValues(packageJson, (value, packageJsonKey) => {
+        if (["dependencies", "devDependencies", "peerDependencies"].includes(packageJsonKey)) {
+            const dependencyRecord = value;
+            return _.mapValues(dependencyRecord, (version: string, dependencyName: string) => {
+                const newDependency = dependencies[dependencyName];
+
+                if (!newDependency || version.includes("workspace")) {
+                    return version;
+                }
+
+                return newDependency.version;
+            });
+        }
+        return value;
+    }) as PackageJson;
+}
 
 async function main(): Promise<void> {
     const versionsJson: RushCheckResult = await $`rush check --json`.json();
     const rushJson: RushJson = await $`rush list --json`.json();
 
-    for (const mismatch of versionsJson.mismatchedVersions) {
-        const version = await askVersion(mismatch);
-    }
+    const dependencies = await async
+        .mapSeries(versionsJson.mismatchedVersions, askVersion)
+        .then((deps) => deps.filter((d) => d.version !== "skip"))
+        .then(groupByDependency);
+
+    await async.eachSeries(
+        rushJson.projects,
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        async (project) => {
+            const packageJsonPath = `${project.fullPath}/package.json`;
+            const packageJson = (await fs.readJson(packageJsonPath)) as unknown as PackageJson;
+            const updated = updateVersions(dependencies, packageJson);
+            await fs.writeJson(packageJsonPath, updated);
+        }
+    );
 }
 
 main().catch((e) => console.error(e));
