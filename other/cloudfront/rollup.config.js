0571fe7d88550a4dcb2c5651c50c7f9e3da50a94
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-02-28 15:28:59 +0200

    Update repo state and update rollup scripts

--- other/cloudfront/rollup.config.js
+++ other/cloudfront/rollup.config.js
@@ -9,16 +9,20 @@ const excludedFiles = [
   "lambda-creator",
 ];
 
+const outputDir = "dist";
+
 const inputs = (await globby([`src/lambda/**/*.ts`])).filter((input) => {
   return !excludedFiles.some((file) => input.includes(file));
 });
 
-const defaultPlugins = [
+const defaultPlugins = (tsConfigOverrides) => [
   nodeResolve({
     exportConditions: ["node", "default", "module", "import", "require"],
     preferBuiltins: true,
   }),
-  typescript(),
+  typescript({
+    compilerOptions: tsConfigOverrides,
+  }),
   commonjs(),
   json(),
 ];
@@ -32,10 +36,20 @@ const excludeTerser = [
 export default inputs.map((input) => {
   const isOutputEsm = false; // moduleJs.some((esm) => input.includes(esm))
   const outputFile = isOutputEsm
-    ? input.replace("src/", "dist/").replace("ts", "mjs")
-    : input.replace("src/", "dist/").replace("ts", "cjs");
+    ? input.replace("src/", `${outputDir}/`).replace("ts", "mjs")
+    : input.replace("src/", `${outputDir}/`).replace("ts", "cjs");
 
-  const plugins = [...defaultPlugins];
+  const outputFileDirectories = input.replace("src/", `${outputDir}/`).split(
+    "/",
+  ).slice(0, -1);
+  const fileLocation = outputFileDirectories.join("/");
+
+  const tsConfig = {
+    outDir: fileLocation,
+    declarationDir: fileLocation,
+  };
+
+  const plugins = [...defaultPlugins(tsConfig)];
 
   if (!excludeTerser.some((exclude) => input.includes(exclude))) {
     plugins.push(terser());
