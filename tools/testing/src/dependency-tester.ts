14e66939251a47487ef1a770ad0b6de8cd0b6b31
teijosol <teijo.ronka@solita.fi>
2025-05-15 15:56:16 +0300

    added test for cdk-lib dependency in lambdas

--- tools/testing/src/dependency-tester.ts
+++ tools/testing/src/dependency-tester.ts
@@ -7,8 +7,16 @@ export class DependencyTester {
     this._instance = instance;
   }
 
-  private checkWhiteList(whitelist: string[], circulars: string[]): string[] {
-    return whitelist.filter((whitelisted) => !circulars.includes(whitelisted));
+  private static checkWhiteList(whitelist: string[], items: string[]): void {
+    const missing = whitelist.filter((whitelisted) =>
+      !items.includes(whitelisted)
+    );
+
+    if (missing.length !== 0) {
+      throw new Error(
+        "Whitelisted dependencies not found:" + JSON.stringify(missing),
+      );
+    }
   }
 
   assertNoCircularDependencies(whitelist: string[] = []): void {
@@ -18,15 +26,10 @@ export class DependencyTester {
     );
 
     if (errors.length !== 0) {
-      throw new Error("Circular dependencies found!");
+      throw new Error("Circular dependencies found:" + errors.join("\n"));
     }
 
-    const missing = this.checkWhiteList(whitelist, circulars);
-    if (missing.length !== 0) {
-      throw new Error(
-        "Whitelisted dependencies not found:" + JSON.stringify(missing),
-      );
-    }
+    DependencyTester.checkWhiteList(whitelist, circulars);
   }
 
   assertNoOrphans(whitelist: string[] = []): void {
@@ -37,12 +40,43 @@ export class DependencyTester {
       throw new Error("Orphans found!");
     }
 
-    const missing = this.checkWhiteList(whitelist, orphans);
-    if (missing.length !== 0) {
+    DependencyTester.checkWhiteList(whitelist, orphans);
+  }
+
+  static async assertNoCdkLibDependenciesInLambdas(
+    paths: string | string[],
+    fileExtensions: string[] = ["js"],
+    whiteList: string[] = [],
+  ): Promise<void> {
+    const instance = await madge(paths, {
+      includeNpm: true,
+      fileExtensions,
+    });
+
+    const errors = [];
+    const fields = [];
+
+    for (const [field, list] of Object.entries(instance.obj())) {
+      if (field.includes("lambda/")) {
+        const deps = list.filter((d) => d.includes("aws-cdk-lib"));
+
+        if (deps.length > 0 && !whiteList.includes(field)) {
+          errors.push(
+            `${field} has aws-lib dependency: ${JSON.stringify(deps)}`,
+          );
+        }
+
+        fields.push(field);
+      }
+    }
+
+    if (errors.length > 0) {
       throw new Error(
-        "Whitelisted orphans not found:" + JSON.stringify(missing),
+        "Lambdas have cdk-lib dependencies: " + errors.join("\n"),
       );
     }
+
+    DependencyTester.checkWhiteList(whiteList, fields);
   }
 
   static async create(
