5883be581f001d10030aad847a124e23e28dbf49
Ilari Junnonen <ilari.junnonen@solita.fi>
2025-11-21 15:39:33 +0200

    DPO-3416 throw new error from fetchData

--- rail/yearly-plans/src/service/yearly-plans-service.ts
+++ rail/yearly-plans/src/service/yearly-plans-service.ts
@@ -1,28 +1,26 @@
-import ky from "ky";
 import { logger } from "@digitraffic/common/dist/aws/runtime/dt-logger-default";
 import { logException } from "@digitraffic/common/dist/utils/logging";
+import ky from "ky";
 
 export async function fetchData<T>(url: string, apiKey: string): Promise<T> {
   const start = Date.now();
 
   logger.info({
     method: "YearlyPlansService.fetchData",
-    message: `Fetching from ${url}`
+    message: `Fetching from ${url}`,
   });
 
   try {
     const response = await ky.get(url, {
       timeout: 30000,
       headers: {
-        "API-Key": apiKey
+        "API-Key": apiKey,
       },
     });
     return await response.json<T>();
-
   } catch (error) {
     logException(logger, error);
-    throw error;
-
+    throw new Error("Failed to fetch data from source system");
   } finally {
     logger.info({
       method: "YearlyPlansService.fetchData",
