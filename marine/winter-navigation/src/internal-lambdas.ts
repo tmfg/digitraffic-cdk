4c0603e68f2aa83ae3717b9b0835085b4c9c7633
Ilari Junnonen <ilari.junnonen@solita.fi>
2025-11-28 14:33:48 +0200

    update wn data every 5min

--- marine/winter-navigation/src/internal-lambdas.ts
+++ marine/winter-navigation/src/internal-lambdas.ts
@@ -1,14 +1,12 @@
-import type { DigitrafficStack } from "@digitraffic/common/dist/aws/infra/stack/stack";
-import {
-  MonitoredDBFunction,
-  type MonitoredFunction,
-} from "@digitraffic/common/dist/aws/infra/stack/monitoredfunction";
 import { Scheduler } from "@digitraffic/common/dist/aws/infra/scheduler";
+import type { MonitoredFunction } from "@digitraffic/common/dist/aws/infra/stack/monitoredfunction";
+import { MonitoredDBFunction } from "@digitraffic/common/dist/aws/infra/stack/monitoredfunction";
+import type { DigitrafficStack } from "@digitraffic/common/dist/aws/infra/stack/stack";
 
 export function create(stack: DigitrafficStack): void {
   const updateLambda = createUpdateDataLambda(stack);
 
-  Scheduler.everyMinutes(stack, "UpdateDataRule", 1, updateLambda);
+  Scheduler.everyMinutes(stack, "UpdateDataRule", 5, updateLambda);
 }
 
 function createUpdateDataLambda(stack: DigitrafficStack): MonitoredFunction {
