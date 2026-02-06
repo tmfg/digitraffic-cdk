bb764434bdd5ddd0e9e2ba49a4465fc9088bca1d
teijosol <teijo.ronka@solita.fi>
2026-02-06 12:10:48 +0200

    fix namespaces for vs-datex2

--- road/data-upload/src/service/rtti.ts
+++ road/data-upload/src/service/rtti.ts
@@ -1,87 +1,102 @@
-import { logger } from "@digitraffic/common/dist/aws/runtime/dt-logger-default";
-import { inDatabase, type DTDatabase } from "@digitraffic/common/dist/database/database";
-import { getNewData, updateStatus, type DataIncomingDb } from "../dao/data.js";
-import { Datex2Version, SOURCES, TYPES } from "../model/types.js";
-import { logException } from "@digitraffic/common/dist/utils/logging";
-import { parseRtti35 } from "./rtti-35-parser.js";
-import { updateRtti } from "../dao/rtti.js";
 import { SQS } from "@aws-sdk/client-sqs";
+import { logger } from "@digitraffic/common/dist/aws/runtime/dt-logger-default";
+import type { DTDatabase } from "@digitraffic/common/dist/database/database";
+import { inDatabase } from "@digitraffic/common/dist/database/database";
+import { logException } from "@digitraffic/common/dist/utils/logging";
 import { getEnvVariableSafe } from "@digitraffic/common/dist/utils/utils";
+import type { DataIncomingDb } from "../dao/data.js";
+import { getNewData, updateStatus } from "../dao/data.js";
+import { updateRtti } from "../dao/rtti.js";
+import { Datex2Version, SOURCES, TYPES } from "../model/types.js";
+import { parseRtti35 } from "./rtti-35-parser.js";
 
 const SQS_URL = getEnvVariableSafe("MQTT_QUEUE_URL");
 const sqs = new SQS({});
 
 export async function handleRttiMessages(): Promise<void> {
-    const method = "RttiService.handleRttiMessages" as const;
+  const method = "RttiService.handleRttiMessages" as const;
 
-    await inDatabase(async (db) => {
-        const unhandled = await getNewData(db, SOURCES.TOPIC, [
-            TYPES.RTTI_DATEX2_XML,
-        ]);
+  await inDatabase(async (db) => {
+    const unhandled = await getNewData(db, SOURCES.TOPIC, [
+      TYPES.RTTI_DATEX2_XML,
+    ]);
 
-        await Promise.allSettled(unhandled.map(async (data) => {
-            logger.debug(`Handling variable sign data id ${data.data_id}`);
+    await Promise.allSettled(
+      unhandled.map(async (data) => {
+        logger.debug(`Handling rtti data id ${data.data_id}`);
 
-            try {
-                await handleRtti(db, data);
+        try {
+          await handleRtti(db, data);
 
-                await updateStatus(db, data.data_id, "PROCESSED");
-            } catch (error) {
-                logger.debug(data.data);
+          await updateStatus(db, data.data_id, "PROCESSED");
+        } catch (error) {
+          logger.debug(data.data);
 
-                logger.error({
-                    method,
-                    error,
-                });
+          logger.error({
+            method,
+            error,
+          });
 
-                await updateStatus(db, data.data_id, "FAILED");
-            }
-        }));
-    });
+          await updateStatus(db, data.data_id, "FAILED");
+        }
+      }),
+    );
+  });
 }
 async function handleRtti(db: DTDatabase, data: DataIncomingDb) {
-    const method = "RttiService.handleRtti" as const;
-    const started = Date.now();
+  const method = "RttiService.handleRtti" as const;
+  const started = Date.now();
 
-    let unknownCount = 0;
-    let updated35Count = 0;
-    let error35Count = 0;
-    
-    switch (data.version) {
-        case "3.5":
-            const s = await parseRtti35(data.data);
+  let unknownCount = 0;
+  let updated35Count = 0;
+  let error35Count = 0;
 
-            try {
-                logger.debug("Updating RTTI situation " + JSON.stringify(s));
-                updated35Count++;
-                await updateRtti(db, s.situationId, s.type, s.publicationTime, s.geometry, s.startTime, s.endTime, s.isSrti, s.message);
+  switch (data.version) {
+    case "3.5": {
+      const s = await parseRtti35(data.data);
 
-                if (SQS_URL.result === "ok") {
-                    await sqs.sendMessage({
-                        MessageBody: JSON.stringify({
-                            id: s.situationId
-                        }),
-                        QueueUrl: SQS_URL.value,
-                    });
-                } else {
-                    logger.debug("Skipping MQTT message sending, no SQS URL configured");
-                }
-            } catch (error) {
-                logException(logger, error);
-                error35Count++;
-            }
+      try {
+        logger.debug("Updating RTTI situation " + JSON.stringify(s));
+        updated35Count++;
+        await updateRtti(
+          db,
+          s.situationId,
+          s.type,
+          s.publicationTime,
+          s.geometry,
+          s.startTime,
+          s.endTime,
+          s.isSrti,
+          s.message,
+        );
 
-            break;
-        default:  
-            unknownCount++;
+        if (SQS_URL.result === "ok") {
+          await sqs.sendMessage({
+            MessageBody: JSON.stringify({
+              id: s.situationId,
+            }),
+            QueueUrl: SQS_URL.value,
+          });
+        } else {
+          logger.debug("Skipping MQTT message sending, no SQS URL configured");
+        }
+      } catch (error) {
+        logException(logger, error);
+        error35Count++;
+      }
 
-            logger.error({
-                method,
-                message: `Unknown rtti datex version ${data.version}`,
-            });  
+      break;
     }
+    default:
+      unknownCount++;
 
-    logger.info({
+      logger.error({
+        method,
+        message: `Unknown rtti datex version ${data.version}`,
+      });
+  }
+
+  logger.info({
     method,
     customDatexVersion: Datex2Version["3.5"],
     customUpdatedCount: updated35Count,
