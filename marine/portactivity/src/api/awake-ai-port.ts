b09f2d995b22e62df10ffbd8c317cfdeddc56745
Peter Skogberg <peter.skogberg@solita.fi>
2026-06-18 08:44:51 +0300

    DPO-4655 testing incorrect header text

--- marine/portactivity/src/api/awake-ai-port.ts
+++ marine/portactivity/src/api/awake-ai-port.ts
@@ -81,7 +81,7 @@ export class AwakeAiPortApi {
       const response = await ky
         .get(url, {
           headers: {
-            Authorization: "Bearer " + oAuthToken.access_token,
+            Authorization: "Bearer XYZ " + oAuthToken.access_token,
             Accept: MediaType.APPLICATION_JSON,
           },
           retry: 0,
