098d20c2bbeee332d25cd9bb9b3db6c05796e30b
Peter Skogberg <peter.skogberg@solita.fi>
2026-08-25 14:33:44 +0300

    DPO-1972 test fix

--- rail/data-dump/src/lambda/tests/test_dump_train_locations.py
+++ rail/data-dump/src/lambda/tests/test_dump_train_locations.py
@@ -118,8 +118,8 @@ class TestDumpTrainLocations(unittest.TestCase):
 
             with rm.Mocker() as m:
                 m.get(f"{API_BASE}/trains/2025-12-31", json=MOCK_TRAINS)
-                m.get(f"{API_BASE}/train-locations/2025-12-31/101", json=MOCK_LOCATIONS_103)
-                m.get(f"{API_BASE}/train-locations/2025-12-31/102", json=MOCK_LOCATIONS_104)
+                m.get(f"{API_BASE}/train-locations/2025-12-31/103", json=MOCK_LOCATIONS_103)
+                m.get(f"{API_BASE}/train-locations/2025-12-31/104", json=MOCK_LOCATIONS_104)
 
                 with patch.dict(os.environ, {"DUMP_BUCKET_NAME": BUCKET_NAME}):
                     # When
