0f5a3cc8c9ec2226a499ef8fbeb2e7602dcd7b40
Peter Skogberg <peter.skogberg@solita.fi>
2026-09-01 12:18:57 +0300

    DPO-1972 geoJson to separate file

--- rail/data-dump/src/lambda/dump-train-locations/dump-train-locations.py
+++ rail/data-dump/src/lambda/dump-train-locations/dump-train-locations.py
@@ -91,12 +91,18 @@ def writeTrainLocationsToFile(departureDate):
     fileName = f'train-locations-{departureDate}.json'
     filePath = f'/tmp/{fileName}'
 
-    # Write locations incrementally — stream each train's locations directly to file
+    fileNameForGeoJson = f'train-locations-{departureDate}-geojson.json'
+    filePathForGeoJson = f'/tmp/{fileNameForGeoJson}'
+
+    # Write locations incrementally — stream each train's locations directly to files
     # instead of accumulating millions of records in memory
-    with open(filePath, 'w') as f:
-        f.write('{')
-        f.write('"type": "FeatureCollection",')
-        f.write('"features": [')
+    with open(filePath, 'w') as f, open(filePathForGeoJson, 'w') as fileForGeoJson:
+        f.write('[')
+
+        fileForGeoJson.write('{')
+        fileForGeoJson.write('"type": "FeatureCollection",')
+        fileForGeoJson.write('"features": [')
+
         first = True
         for i, trainNumber in enumerate(trainNumbers):
             if (i + 1) % 50 == 0 or i == 0:
@@ -107,32 +113,42 @@ def writeTrainLocationsToFile(departureDate):
             for loc in locations:
                 if not first:
                     f.write(',')
+                    fileForGeoJson.write(',')
                 if checkThatJsonHasValuesRequiredByGeoJson(loc):
-                  locationGeoJson = createGeoJsonFeaturePart(loc)
-                  json.dump(locationGeoJson, f)
+                    locationGeoJson = createGeoJsonFeaturePart(loc)
+                    json.dump(locationGeoJson, fileForGeoJson)
                 else:
-                  return False
+                    return False
+
+                json.dump(loc, f)
                 first = False
                 recordCount += 1
         f.write(']')
-        f.write('}')
+        fileForGeoJson.write(']')
+        fileForGeoJson.write('}')
 
     logger.info('Wrote location records', count=recordCount, file_path=filePath)
-
-    s3_fileName = f'digitraffic-rata-train-locations-{departureDate}.zip'
-    s3_filePath = f'/tmp/{s3_fileName}'
 
     logger.info('Zipping file', file_name=fileName)
     with zipfile.ZipFile(s3_filePath, 'w', zipfile.ZIP_DEFLATED) as zip:
         zip.write(filePath, fileName)
+        zip.write(filePathForGeoJson, fileNameForGeoJson)
 
     # Remove uncompressed file to free /tmp space before upload
     os.remove(filePath)
+    os.remove(filePathForGeoJson)
+
+    s3_fileName = f'digitraffic-rata-train-locations-{departureDate}.zip'
+    s3_filePath = f'/tmp/{s3_fileName}'
+
+    s3_fileNameForGeoJson = f'digitraffic-rata-train-locations-{departureDate}.zip'
+    s3_filePathForGeoJson = f'/tmp/{s3_fileNameForGeoJson}'
 
     logger.info('Uploading to S3', s3_file_name=s3_fileName)
     s3 = boto3.client('s3')
     bucket_name = os.environ['DUMP_BUCKET_NAME']
 
     s3.upload_file(s3_filePath, bucket_name, s3_fileName)
+    s3.upload_file(s3_filePathForGeoJson, bucket_name, s3_fileNameForGeoJson)
 
     return True
